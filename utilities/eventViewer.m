3e2b1839875e1266134326b42d8068805ed65b7b
Jason Farquhar <jdrf@zepler.org>
2013-11-27 13:15:04 +0100

    bug fix
    Former-commit-id: 0f8beb71d5d7a525717b3e6a759c34f27e0e80c4

--- utilities/eventViewer.m
+++ utilities/eventViewer.m
@@ -1,4 +1,17 @@
 run ../utilities/initPaths;
+buffhost='localhost'; buffport=1972;
+% wait for valid header
+hdr=[];
+while ( isempty(hdr) || ~isstruct(hdr) || (hdr.nchans==0) ) % wait for the buffer to contain valid data
+  try 
+    hdr=buffer('get_hdr',[],buffhost,buffport); 
+  catch
+    hdr=[];
+    fprintf('Invalid header info... waiting.\n');
+  end;
+  pause(1);
+end;
+
 nevents=[];
 while ( true )
   [events,nevents]=buffer_newevents([],[],nevents,buffhost,buffport);
