b3ad5ff23ca5ab7d486b39456f38a65eb151d57a
Jason Farquhar <jdrf@zepler.org>
2013-12-02 13:46:43 +0100

    help text and make buffer-struct compatatible
    Former-commit-id: fa0d762e6d7031744f32d5f49f266bdbc13fe6b0

--- signalProc/buffer_apply_ersp_clsfr.m
+++ signalProc/buffer_apply_ersp_clsfr.m
@@ -4,7 +4,11 @@ function [f,fraw,p]=buffer_apply_ersp_clsfr(X,clsfr,verb)
 %  f=buffer_apply_erp_clsfr(X,clsfr,varargin)
 %
 % Inputs:
-%  X - [ch x time x epoch ] dataset 
+%   X -- [ch x time x epoch] data
+%        OR
+%        [struct epoch x 1] where the struct contains a buf field of buffer data
+%        OR
+%        {[float ch x time] epoch x 1} cell array of data
 %  clsfr - [struct] classifier structure
 %  verb  - [int] verbosity level (0)
 % Output:
@@ -12,6 +16,16 @@ function [f,fraw,p]=buffer_apply_ersp_clsfr(X,clsfr,verb)
 %  fraw - [size(X,dim) x nSp] set of pre-binary sub-problem decision values
 %  p     - [size(X,epoch) x nCls] the classifier's assessment of the probablility of each class
 if( nargin<3 || isempty(verb))  verb=0; end;
+% extract the data - from field begining with trainingData
+if ( iscell(X) ) 
+  if ( isnumeric(X{1}) ) 
+    X=cat(3,X{:});
+  else
+    error('Unrecognised data format!');
+  end
+elseif ( isstruct(X) )
+  X=cat(3,X.buf);
+end 
 [f, fraw, p]=apply_ersp_clsfr(X,clsfr,verb);
 if ( verb>0 ) fprintf('Classifier prediction:  %g %g\n', f,p); end;
 return;
