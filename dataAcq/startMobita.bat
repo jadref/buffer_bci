ecfd1bb96ee8c5457b982049b55a2bc1a16ab683
Jason Farquhar <jdrf@zepler.org>
2014-01-17 22:42:01 +0100

    mobita drivers update
    Former-commit-id: 4046ed398e74d729a0e968b3ce2d963c6627b745

--- dataAcq/startMobita.bat
+++ dataAcq/startMobita.bat
@@ -3,4 +3,4 @@ rem Search for the buffer executable
 if exist "%batdir%buffer\bin\win32\mobita2ft.exe" ( set buffexe="%batdir%buffer\bin\win32\mobita2ft.exe" )
 if exist "%batdir%buffer\win32\mobita2ft.exe" ( set buffexe="%batdir%buffer\win32\mobita2ft.exe" )
 if exist "%batdir%mobita2ft.exe" ( set buffexe="%batdir%mobita2ft.exe" )
-start /b "buffer" %buffexe %1 %2 %3
+start /b "buffer" %buffexe% %1 %2 %3
