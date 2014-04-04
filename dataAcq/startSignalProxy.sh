a03db31ed81b1c375730a78df053d781f87ab6dd
Jason Farquhar <jdrf@zepler.org>
2014-04-04 19:31:14 +0200

    octave compat
    Former-commit-id: fdf6545c57ad3d05b2099986216d8c259db6f61e

--- dataAcq/startSignalProxy.sh
+++ dataAcq/startSignalProxy.sh
@@ -2,7 +2,7 @@
 cd `dirname ${BASH_SOURCE[0]}`
 source ../utilities/findMatlab.sh
 cat <<EOF | $matexe -nodesktop  -nosplash
-run ../utilities/initPaths; 
+run ../utilities/initPaths.m; 
 buffer_signalproxy('localhost',1972,'stimEventRate',0,'queueEventRate',0);
 quit;
 EOF
