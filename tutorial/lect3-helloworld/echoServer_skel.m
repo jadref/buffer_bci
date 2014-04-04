a687f882d9d2d6d1ff77bf346c8682221ac6fcdd
Jason Farquhar <jdrf@zepler.org>
2014-04-04 19:34:05 +0200

    echo task example code
    Former-commit-id: 6bb4f0b0ba887d32ccf2d8bd4e6991677484479e

---
+++ tutorial/lect3-helloworld/echoServer_skel.m
@@ -0,0 +1,31 @@
+run ../../utilities/initPaths.m;
+
+buffhost='localhost';buffport=1972;
+global ft_buff; ft_buff=struct('host',buffhost,'port',buffport);
+% wait for the buffer to return valid header information
+hdr=[];
+while ( isempty(hdr) || ~isstruct(hdr) || (hdr.nchans==0) ) % wait for the buffer to contain valid data
+  try 
+    hdr=buffer('get_hdr',[],buffhost,buffport); 
+  catch
+    hdr=[];
+    fprintf('Invalid header info... waiting.\n');
+  end;
+  pause(1);
+end;
+% set the real-time-clock to use
+initgetwTime();
+initsleepSec();
+
+% ----------------------------------------------------------------------------
+%    FILL IN YOUR CODE BELOW HERE
+% ----------------------------------------------------------------------------
+
+% wait for new events of a particular type
+state=[]; % initial state
+[events,state]=buffer_newevents(buffhost,buffport,state,'echo'); % wait for next echo event
+
+% send an ack event with the same value, for each recieved event
+for ei=1:numel(events);
+  sendEvent('ack',events(ei).value);
+end
