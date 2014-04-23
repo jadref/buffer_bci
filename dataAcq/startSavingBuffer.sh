eb6e234c96a003001a627a7e512d60fdd21bc0fd
Jason Farquhar <jdrf@zepler.org>
2014-04-23 10:29:24 +0200

    batch files changes
    Former-commit-id: b02896df7ac3fadb6cf30da6c740e35649226706

--- dataAcq/startSavingBuffer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd `dirname ${BASH_SOURCE[0]}`
-savedir="$HOME/output/RacingGame/$$"
-echo SavingDir: $savedir
-if [ `uname -s`=='Linux' ]; then
-	./glnx86/recording $savedir 1972 
-else
-	./glnx86/recording $savedir 1972
-fi
