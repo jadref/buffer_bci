f26541dff5324c0f4a84d847a8ac8d1a645fa280
Jason Farquhar <jdrf@zepler.org>
2014-05-09 17:02:45 +0200

    exercise solutions
    Former-commit-id: d49afe9161a4739b739478a8f0a5c3614fcb6990

---
+++ tutorial/lect3-helloworld/erpviewerStimulus_soln.m
@@ -0,0 +1,72 @@
+run ../utilities/initPaths.m;
+
+buffhost='localhost';buffport=1972;
+% wait for the buffer to return valid header information
+hdr=[];
+while ( isempty(hdr) || ~isstruct(hdr) || (hdr.nchans==0) ) % wait for the buffer to contain valid data
+  try 
+    hdr=buffer('get_hdr',[],buffhost,buffport); 
+  catch
+    hdr=[];
+    fprintf('Invalid header info... waiting.\n');
+  end;
+  pause(1);
+end;
+
+% set the real-time-clock to use
+initgetwTime();
+initsleepSec();
+
+% make the target sequence
+nSeq=5;
+nEpoch=10;
+stimSeq=zeros(nEpoch,nSeq);
+for si=1:nSeq;
+  [ans,ans,ans,stimCode]=mkStimSeqRand(2,nEpoch,[],0);
+  stimSeq(:,si)=stimCode;
+end
+interSeqDuration=3;
+interEpochDuration=1;
+
+% make the stimulus
+clf;
+set(gcf,'color',[0 0 0],'toolbar','none','menubar','none'); % black figure
+set(gca,'visible','off','color',[0 0 0]); % black axes
+h=rectangle('curvature',[1 1],'position',[.25 .25 .5 .5],'facecolor',[.5 .5 .5]);
+set(h,'visible','off');
+
+% play the stimulus
+sendEvent('stimulus.sequences','start');
+for si=1:size(stimSeq,2);
+  sequence=stimSeq(:,si);
+      
+  % reset the cue and fixation point to indicate trial has finished  
+  set(h,'visible','off');
+  drawnow;
+  sendEvent('stimulus.sequence',sequence);
+  
+  % loop over events in the sequence
+  set(h,'visible','on');
+  drawnow;
+  for ci=1:numel(sequence);
+    epoch = sequence(ci);
+    sendEvent('stimulus.epoch',epoch);
+    if ( epoch==1 ) 
+      set(h,'facecolor',[1 1 1]);
+    else
+      set(h,'facecolor',[.5 .5 .5]);
+    end
+    drawnow;
+    fprintf('%d',epoch);
+    sleepSec(interEpochDuration);
+  end
+  sleepSec(interSeqDuration);
+  
+  % wait for a key press
+  msg=msgbox({'Press OK to continue'},'Continue?');while ishandle(msg); pause(.2); end;
+  
+end % sequences
+% end training marker
+sendEvent('stimulus.sequences','end');
+% tell user we're done
+msg=msgbox({'Thanks for taking part!'},'Continue?');while ishandle(msg); pause(.2); end;
---
+++ tutorial/lect3-helloworld/runSentences_soln.m
@@ -0,0 +1,58 @@
+run ../../utilities/initPaths.m;
+
+buffhost='localhost';buffport=1972;
+% wait for the buffer to return valid header information
+hdr=[];
+while ( isempty(hdr) || ~isstruct(hdr) || (hdr.nchans==0) ) % wait for the buffer to contain valid data
+  try 
+    hdr=buffer('get_hdr',[],buffhost,buffport); 
+  catch
+    hdr=[];
+    fprintf('Invalid header info... waiting.\n');
+  end;
+  pause(1);
+end;
+
+% set the real-time-clock to use
+initgetwTime();
+initsleepSec();
+
+% make the target sequence
+sentences={'hello world','this is new!','BCI is fun!'};
+interSentenceDuration=3;
+interCharDuration=1;
+
+% make the stimulus
+clf;
+set(gcf,'color',[0 0 0],'toolbar','none','menubar','none'); % black figure
+set(gca,'visible','off','color',[0 0 0]); % black axes
+h=text(.5,.5,'text','HorizontalAlignment','center','VerticalAlignment','middle',...
+       'FontUnits','normalized','fontsize',.2,'color',[1 1 1],'visible','off'); 
+
+% play the stimulus
+sendEvent('stimulus.sentences','start');
+for si=1:numel(sentences);
+  sentence=sentences{si};
+      
+  % reset the cue and fixation point to indicate trial has finished  
+  set(h,'visible','off');
+  drawnow;
+  sendEvent('stimulus.sentence',sentence);
+    
+  % loop over characters in the sentence
+  for ci=1:numel(sentence);
+    char = sentence(ci);
+    sendEvent('stimulus.character',char);
+    set(h,'string',sentence(1:ci),'visible','on');drawnow;
+    sleepSec(interCharDuration);
+  end
+  sleepSec(interSentenceDuration);
+  
+  % wait for a key press
+  msg=msgbox({'Press OK to continue'},'Continue?');while ishandle(msg); pause(.2); end;
+  
+end % sequences
+% end training marker
+sendEvent('stimulus.sentences','end');
+% tell user we're done
+msg=msgbox({'Thanks for taking part!'},'Continue?');while ishandle(msg); pause(.2); end;
