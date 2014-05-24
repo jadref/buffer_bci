0de065075efb6a7b7fd3ab0b1c7d85526e21eb62
Jason Farquhar <jdrf@zepler.org>
2014-05-24 03:32:15 +0200

    more muse fixes
    Former-commit-id: 9e17cda5fc97a2ffa90741dddff7dd47b64b5794

--- dataAcq/osc/compile.sh
+++ dataAcq/osc/compile.sh
@@ -1,3 +1,3 @@
-javac -classpath ../buffer/java/Buffer.jar ../buffer/JavaOSC/modules/core/src/main/java/com/illposed/osc/utility/*.java ../buffer/JavaOSC/modules/core/src/main/java/com/illposed/osc/*.java osc2ft.java
+javac -source 1.5 -target 1.5 -classpath ../buffer/java/Buffer.jar JavaOSC/modules/core/src/main/java/com/illposed/osc/utility/*.java JavaOSC/modules/core/src/main/java/com/illposed/osc/*.java osc2ft.java
 # to run use:
-#java -cp ../../dataAcq/buffer/java/Buffer.jar:.:../buffer/JavaOSC/modules/core/src/main/java osc2ft /data:777
+#java -cp ../dataAcq/buffer/java/Buffer.jar:.:JavaOSC.jar osc2ft /data:777
--- dataAcq/startMuse.sh
+++ dataAcq/startMuse.sh
@@ -6,7 +6,7 @@ oscport=1234
 oscdevice=ffc3
 # 1) run the OSC -> ft_buffer converter with parameters for the MUSE  !in the background!
 #    This will then wait for data from the MUSE and connection to the buffer
-java -cp buffer/java/Buffer.jar:osc/JavaOSC/JavaOSC.jar:osc osc2ft /muse/eeg/raw:${oscport} localhost:1972 6 500 1 10 &
+java -cp buffer/java/Buffer.jar:osc/JavaOSC.jar:osc osc2ft /muse/eeg/raw:${oscport} localhost:1972 6 500 1 10 &
 
 if [ `uname -s` == 'Linux' ]; then
 	 echo Sorry Linux isnt supported yet!
@@ -22,7 +22,7 @@ else
 fi
 # 2) run the muse-io driver
 $buffexe --device $oscdevice --preset ab --osc osc.udp://localhost:$oscport
-if [ $? -neq 0 ]; then
+if [ $? -ne 0 ]; then
 	 echo "Error couldn't connect to the MUSE"
 	 kill %1 # kill the background java job
 	 exit -1
