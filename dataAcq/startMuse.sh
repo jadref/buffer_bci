a19b464550ae7d031909691b4dec72f6c1c4f6d7
Jason Farquhar <jdrf@zepler.org>
2014-05-24 12:52:59 +0200

    kill java on exit
    Former-commit-id: f4235c57c0e222198282213c96f6a8e4d731c1e4

--- dataAcq/startMuse.sh
+++ dataAcq/startMuse.sh
@@ -7,9 +7,13 @@ oscdevice=ffc3
 # 1) run the OSC -> ft_buffer converter with parameters for the MUSE  !in the background!
 #    This will then wait for data from the MUSE and connection to the buffer
 java -cp buffer/java/Buffer.jar:osc/JavaOSC.jar:osc osc2ft /muse/eeg/raw:${oscport} localhost:1972 6 500 1 10 &
+# catch ctrl-c and kill the java too
+trap 'kill %1' SIGTERM SIGINT SIGHUP
 
 if [ `uname -s` == 'Linux' ]; then
 	 echo Sorry Linux isnt supported yet!
+	 kill %1 # kill the background java job
+	 exit -1	 
 else
    if [ -r $buffdir/buffer/bin/maci/muse-io ]; then
     # Argh, annoyingly the MUSE driver only works if run in it's own directory
