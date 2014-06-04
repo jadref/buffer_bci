83e57d4d9fc4413bd556d8a8c7817a87c9677bcd
Jason Farquhar <jdrf@zepler.org>
2014-06-04 16:03:51 +0200

    improve octave support
    Former-commit-id: 8e8162523ea1903d8991d4aae71760fa64a8ef82

--- dataAcq/rttTest.m
+++ dataAcq/rttTest.m
@@ -1,4 +1,4 @@
-run ../utilities/initPaths
+run ../utilities/initPaths.m
 global getwTime; getwTime=[];initgetwTime();
 buffhost='localhost';buffport=1972;
 global ft_buff; ft_buff=struct('host',buffhost,'port',buffport);
