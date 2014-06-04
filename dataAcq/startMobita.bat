a98667c6713d4e4c72895aa96c801f65c4a1a0ba
Jason Farquhar <jdrf@zepler.org>
2014-06-04 16:04:34 +0200

    added high priority to reduce crashes
    Former-commit-id: ac4a999672598751d00bbaaa7f7807b93b8bab49

--- dataAcq/startMobita.bat
+++ dataAcq/startMobita.bat
@@ -4,4 +4,4 @@ if exist "%batdir%buffer\bin\win32\mobita2ft.exe" ( set buffexe="%batdir%buffer\
 if exist "%batdir%buffer\win32\mobita2ft.exe" ( set buffexe="%batdir%buffer\win32\mobita2ft.exe" )
 if exist "%batdir%mobita2ft.exe" ( set buffexe="%batdir%mobita2ft.exe" )
 rem start /b "buffer" %buffexe% %1 %2 %3
-start /b "buffer" %buffexe% 10.11.12.13:4242 localhost:1972 50 4
+start /b "buffer" /HIGH %buffexe% 10.11.12.13:4242 localhost:1972 50 4
