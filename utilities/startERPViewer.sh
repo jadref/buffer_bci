1ca8c198c3eb72c20a41b66977c2ace633f6c08b
Jason Farquhar <jdrf@zepler.org>
2014-06-04 16:06:19 +0200

    improved octave support
    Former-commit-id: cb704c11910f9064a0d2c0d642484e08c64d04ef

--- utilities/startERPViewer.sh
+++ utilities/startERPViewer.sh
@@ -3,7 +3,7 @@ cd `dirname ${BASH_SOURCE[0]}`
 source ../utilities/findMatlab.sh
 if [[ $matexe == *matlab ]]; then  args=-nodesktop; fi
 cat <<EOF | $matexe $args
-run ../utilities/initPaths;
+run ../utilities/initPaths.m;
 [X,Y,key]=erpViewer([],[],'cuePrefix',{'keyboard' 'stimulus'});
 save('erpViewerDat','X','Y','key');
 quit;
--- utilities/startEventViewer.bat
+++ utilities/startEventViewer.bat
@@ -1,4 +1,4 @@
 call ..\utilities\findMatlab.bat
 set batdir=%~dp0
 cd %batdir%
-start "Matlab" %matexe% -nodesktop -nosplash -r "run ../utilities/initPaths;eventViewer();quit;"
+start "Matlab" %matexe% -nodesktop -nosplash -r "run ../utilities/initPaths.m;eventViewer();quit;"
