fe14de494ba411e6be45bb1a152c6ccce239b60b
Jason Farquhar <jdrf@zepler.org>
2014-08-02 14:42:48 +0200

    fix auto matlab search on linux
    Former-commit-id: 8825fd7b28bd1c048e49e9d9dd6cd373890cbcda

--- utilities/findMatlab.sh
+++ utilities/findMatlab.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 export matexe=`which matlab`
-if [ -z "$matexe" ]; then
+if [ ! -f "$matexe" ]; then
     if [ `uname -s` == 'Darwin' ]; then # MAC
         mdirs=`\ls -dt /Applications/MATLAB*`;
-	if [ ! -z "$mdirs" ]; then
-        matexe=${mdirs%%$'\n'*}/bin/matlab;
-	fi
-fi
-if [ -z "$matexe" ]; then  # see if Octave is installed, use it as fall-back
+		  if [ ! -z "$mdirs" ]; then # use the last one in the list
+				matexe=${mdirs%%$'\n'*}/bin/matlab;
+		  fi
+	 fi
+	 if [ ! -f "$matexe" ]; then  # see if Octave is installed, use it as fall-back
 		  export matexe=`which octave`
-    echo $matexe
+		  echo $matexe
     fi
 fi
 export matexe
