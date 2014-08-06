750df89415d3f4c485ec3e9533e70a7b124030c2
Wieke Kanters <kantersw@gmail.com>
2014-08-06 12:15:06 +0200

    Added numpy availibility check.
    Former-commit-id: 342ea4737665ac830be2f55fbbcbb46987b9ed4b

--- dataAcq/buffer/python/setup.py
+++ dataAcq/buffer/python/setup.py
@@ -1,9 +1,15 @@
 from distutils.core import setup
 
-setup(name='FieldTrip',
+try:
+    import numpy
+
+    setup(name='FieldTrip',
       version='1.0',
       description='Python Fieldtrip Client',
       author='S. Klanke',
       url='https://github.com/jadref/buffer_bci/',
-      py_modules = ['FieldTrip'],
+      py_modules = ['FieldTrip']
      )
+except ImportError:
+    print "FieldTrip buffer requires numpy."
+
