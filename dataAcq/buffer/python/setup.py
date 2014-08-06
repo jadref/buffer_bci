dde49e32596f78674ff6d732f80a000d0899a914
Wieke Kanters <kantersw@gmail.com>
2014-08-06 11:51:17 +0200

    Added setup.py to the python FieldTrip buffer client.
    Former-commit-id: 0b9699a7bcaeeed4f52590e8ce1e344f1911f40c

---
+++ dataAcq/buffer/python/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='FieldTrip',
+      version='1.0',
+      description='Python Fieldtrip Client',
+      author='S. Klanke',
+      url='https://github.com/jadref/buffer_bci/',
+      py_modules = ['FieldTrip'],
+     )
