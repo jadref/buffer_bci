06e64d6d7f76fdad25f022d5396a3baa2e6a2872
Wieke Kanters <kantersw@gmail.com>
2014-08-08 19:02:21 +0200

    Added fallback functions for when numpy isn't availeble to FieldTrip.py
    Former-commit-id: 3877a1c931075a6e10ca22c1013016e1284beb42

--- dataAcq/buffer/python/setup.py
+++ dataAcq/buffer/python/setup.py
@@ -3,13 +3,14 @@ from distutils.core import setup
 try:
     import numpy
 
-    setup(name='FieldTrip',
-      version='1.0',
-      description='Python Fieldtrip Client',
-      author='S. Klanke',
-      url='https://github.com/jadref/buffer_bci/',
-      py_modules = ['FieldTrip']
-     )
-except ImportError:
-    print "FieldTrip buffer requires numpy."
 
+except ImportError:
+    print "FieldTrip buffer works better with numpy."
+
+setup(name='FieldTrip',
+  version='1.0',
+  description='Python Fieldtrip Client',
+  author='S. Klanke',
+  url='https://github.com/jadref/buffer_bci/',
+  py_modules = ['FieldTrip']
+ )
