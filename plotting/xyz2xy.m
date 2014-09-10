404ac25be06c69a6f4c21d984d8b507a62b79596
Jason Farquhar <jdrf@zepler.org>
2014-09-10 16:50:53 +0200

    new file for 3d->2d electrode position transformation
    Former-commit-id: 339667c44d8efc90c3ce250d5732440987fdbf72

---
+++ plotting/xyz2xy.m
@@ -0,0 +1,16 @@
+function xy=xyz2xy(xyz)
+% search for center of the circle defining the head
+cent=mean(xyz,2); cent(3)=min(xyz(3,:)); 
+f=inf; fstar=inf; tstar=0; 
+for t=-.5:.05:1; % simple loop to find the right height..
+   cent(3)=t*(max(xyz(3,:))-min(xyz(3,:)))+min(xyz(3,:));
+   r2=sum(repop(xyz,'-',cent).^2); 
+   f=sum((r2-mean(r2)).^2); % objective is variance in distance to the center
+   if( f<fstar ) fstar=f; centstar=cent; end;
+end
+cent=centstar;
+r = abs(max(abs(xyz(3,:)-cent(3)))*1.1); if( r<eps ) r=1; end;  % radius
+h = xyz(3,:)-cent(3);  % height
+rr=sqrt(2*(r.^2-r*h)./(r.^2-h.^2)); % arc-length to radial length ratio
+xy = [(xyz(1,:)-cent(1)).*rr; (xyz(2,:)-cent(2)).*rr];
+return
