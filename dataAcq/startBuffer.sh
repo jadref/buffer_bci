19d7efe9360d451f4ec59bbcabfd90f52bd43134
Jason Farquhar <jdrf@zepler.org>
2014-09-16 08:58:55 +0200

    ask for save location
    Former-commit-id: 4af5f6282ebb4b3feb1b038014c911d7c9b868bf

--- dataAcq/startBuffer.sh
+++ dataAcq/startBuffer.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 buffdir=`dirname $0`
-bciroot=~/output
-subject='test';
-if [ $# -gt 0 ]; then subject=$1; fi 
-session=`date +%y%m%d`
-if [ $# -gt 1 ]; then session=$2; fi
-block=`date +%H%M`
-if [ $# -gt 2 ]; then block=$2; fi
-outdir=$bciroot/$subject/$session/$block
+outdir=
+
+# use GUI to update the save location
+if [ -x startBuffer.py ] && [ ! -z `which python` ]; then
+   outdir=`python startBuffer.py`;
+fi
+
+# fall back code to compute save location
+if [ -z $outdir ] ; then
+	 bciroot=~/output
+	 subject='test';
+	 if [ $# -gt 0 ]; then subject=$1; fi 
+	 session=`date +%y%m%d`
+	 if [ $# -gt 1 ]; then session=$2; fi
+	 block=`date +%H%M`
+	 if [ $# -gt 2 ]; then block=$2; fi
+	 outdir=$bciroot/$subject/$session/$block
+fi
+
 if [ -r $outdir ] ; then # already exists?  add postfix
   outdir=${outdir}_1
 fi
@@ -17,23 +28,23 @@ echo logfile : $logfile
 mkdir -p $outdir
 touch $logfile
 if [ `uname -s` == 'Linux' ]; then
+	 if  [ "`uname -a`" == 'armv6l' ]; then
+		  arch='raspberrypi'
+    else
+		  arch='glnx86';
+   fi
    buffexe=$buffdir'/buffer/bin/saving_buffer_glx32';
    if [ -r $buffdir/recording ]; then
-    buffexe=$buffdir'/recording';
-   fi
-   if [ -r $buffdir/buffer/bin/glnx86/recording ]; then
-	 buffexe=$buffdir'/buffer/bin/glnx86/recording';
-   fi
-   if [ -r $buffdir/buffer/glnx86/recording ]; then
-	 buffexe=$buffdir'/buffer/glnx86/recording';
+      buffexe=$buffdir'/recording';
    fi
 else # Mac
-   buffexe=$buffdir'/buffer/bin/saving_buffer';
-   if [ -r $buffdir/buffer/bin/maci/recording ]; then
-	 buffexe=$buffdir'/buffer/bin/maci/recording'
-   fi
-   if [ -r $buffdir/buffer/maci/recording ]; then
-	 buffexe=$buffdir'/buffer/maci/recording'
-   fi
+	 arch='maci'
+   buffexe=$buffdir"/buffer/bin/saving_buffer";
+fi
+if [ -r $buffdir/buffer/bin/${arch}/recording ]; then
+	 buffexe=$buffdir"/buffer/bin/${arch}/recording";
+fi
+if [ -r $buffdir/buffer/${arch}/recording ]; then
+	 buffexe=$buffdir"/buffer/${arch}/recording";
 fi
 $buffexe ${outdir}/raw_buffer > $logfile 
