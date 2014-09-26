ae35fca6ad552137013a5db314026506667e33b6
Jason Farquhar <jdrf@zepler.org>
2014-09-26 18:02:21 +0200

    robust to complex spectral filter values
    Former-commit-id: 10ee7b55558dd09bf2b656a9cad2497d1ef26cac

--- utilities/sigViewer.m
+++ utilities/sigViewer.m
@@ -427,8 +427,8 @@ function [sigprocopts,damage]=getSigProcOpts(optsFighandles,oldopts)
 sigprocopts.badchrm=get(optsFighandles.badchrm,'value');
 sigprocopts.spatfilttype=get(get(optsFighandles.spatfilt,'SelectedObject'),'String');
 sigprocopts.preproctype=get(get(optsFighandles.preproc,'SelectedObject'),'String');
-sigprocopts.freqbands=[str2num(get(optsFighandles.lowcutoff,'string')) ...
-                    str2num(get(optsFighandles.highcutoff,'string'))];  
+sigprocopts.freqbands=[real(str2num(get(optsFighandles.lowcutoff,'string'))) ...
+                    real(str2num(get(optsFighandles.highcutoff,'string')))];  
 if ( numel(sigprocopts.freqbands)>4 ) sigprocopts.freqbands=sigprocopts.freqbands(1:min(end,4));
 elseif ( numel(sigprocopts.freqbands)<2 ) sigprocopts.freqbands=[];
 end;
