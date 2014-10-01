7f35448f5af8d5e0fceb5442a9e5d0e2e6c68a06
Jason Farquhar <jdrf@zepler.org>
2014-10-01 12:46:21 +0200

    prefer latest version 64bit matlab on windows
    Former-commit-id: f8134af3d795a79b243738e883de7a7e961a8711

--- utilities/findMatlab.bat
+++ utilities/findMatlab.bat
@@ -1,9 +1,9 @@
 set matexe="" 
-FOR /d %%f in ("C:\Program Files\MATLAB\*" "C:\Program Files (x86)\MATLAB\*") DO (
-	 if exist "%%f\bin\matlab.exe" ( 
-	 set matexe="%%f\bin\matlab.exe"
-	 exit /b 
-	 )
+FOR /d %%f in ("C:\Program Files (x86)\MATLAB\*" "C:\Program Files\MATLAB\*" ) DO (
+	 rem if exist "%%f\bin\matlab.exe" ( 
+	 rem set matexe="%%f\bin\matlab.exe"
+	 rem exit /b 
+	 rem )
 )
 
 
