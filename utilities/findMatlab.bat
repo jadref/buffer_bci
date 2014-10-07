e09f659022fb7e8e24666832ae14bfe2549d613d
Jason Farquhar <jdrf@zepler.org>
2014-10-07 09:55:33 +0200

    return the last matlab found -> latest version and 64bit if possible
    Former-commit-id: 9f094be21ccaedbb5823d241be228c4ba3887fca

--- utilities/findMatlab.bat
+++ utilities/findMatlab.bat
@@ -2,7 +2,7 @@ set matexe=""
 FOR /d %%f in ("C:\Program Files\MATLAB\*" "C:\Program Files (x86)\MATLAB\*") DO (
 	 if exist "%%f\bin\matlab.exe" ( 
 	 set matexe="%%f\bin\matlab.exe"
-	 exit /b 
+rem	 exit /b 
 	 )
 )
 
