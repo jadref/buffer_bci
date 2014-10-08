fa143bd13ffc66339999c251b2b14703e4ce6cd1
Jason Farquhar <jdrf@zepler.org>
2014-10-08 12:26:48 +0200

    fix silly bugs in .bat files
    Former-commit-id: 2d7d113f570eafe177bf627636b441a734b716a3

--- dataAcq/startSignalProxy2.bat
+++ dataAcq/startSignalProxy2.bat
@@ -4,4 +4,4 @@ rem Search for the executable
 if exist "%batdir%buffer\bin\win32\csignalproxy.exe" ( set buffexe="%batdir%buffer\bin\win32\csignalproxy.exe" )
 if exist "%batdir%buffer\win32\csignalproxy.exe" ( set buffexe="%batdir%buffer\win32\csignalproxy.exe" )
 if exist "%batdir%csignalproxy.exe" ( set buffexe="csignalproxy.exe" )
-start /b "signalproxy"
+start /b "signalproxy" %buffexe%
--- utilities/findMatlab.bat
+++ utilities/findMatlab.bat
@@ -4,6 +4,7 @@ FOR /d %%f in ("C:\Program Files\MATLAB\*" "C:\Program Files (x86)\MATLAB\*") DO
 	 set matexe="%%f\bin\matlab.exe"
 rem	 exit /b 
 	 )
+)
 
 
 
