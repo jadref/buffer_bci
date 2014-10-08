7fe58d0a91fcb6f741d119b92bea670f3660656e
Jason Farquhar <jdrf@zepler.org>
2014-10-08 12:11:42 +0200

    fast c based signal proxy
    Former-commit-id: 551847a16efdcbb043e3de293d7ce44b8ad395a9

---
+++ dataAcq/startSignalProxy2.bat
@@ -0,0 +1,7 @@
+setlocal enabledelayedexpansion
+set batdir=%~dp0
+rem Search for the executable
+if exist "%batdir%buffer\bin\win32\csignalproxy.exe" ( set buffexe="%batdir%buffer\bin\win32\csignalproxy.exe" )
+if exist "%batdir%buffer\win32\csignalproxy.exe" ( set buffexe="%batdir%buffer\win32\csignalproxy.exe" )
+if exist "%batdir%csignalproxy.exe" ( set buffexe="csignalproxy.exe" )
+start /b "signalproxy"
