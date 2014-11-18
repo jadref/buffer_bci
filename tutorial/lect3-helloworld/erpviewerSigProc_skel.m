ef65a8f93bf10a00fa175668bb9ccd099031de30
Jason Farquhar <jdrf@zepler.org>
2014-11-18 22:05:27 +0100

    updated tutorial examples
    Former-commit-id: c615560d01b4ce7ad6a66cb8f1482997e49899c4

---
+++ tutorial/lect3-helloworld/erpviewerSigProc_skel.m
@@ -0,0 +1,54 @@
+run ../../utilities/initPaths;
+  
+buffhost='localhost';buffport=1972;
+% wait for the buffer to return valid header information
+hdr=[];
+while ( isempty(hdr) || ~isstruct(hdr) || (hdr.nchans==0) ) % wait for the buffer to contain valid data
+  try 
+    hdr=buffer('get_hdr',[],buffhost,buffport); 
+  catch
+    hdr=[];
+    fprintf('Invalid header info... waiting.\n');
+  end;
+  pause(1);
+end;
+
+% set the real-time-clock to use
+initgetwTime;
+initsleepSec;
+
+% get cap layout info
+di = addPosInfo(hdr.channel_names,'1010'); % get 3d-coords
+ch_pos=cat(2,di.extra.pos2d); ch_names=di.vals; iseeg=[di.extra.iseeg]; % extract pos and channels names
+
+trlen_samp = 50; % #samples per epoch
+nSymbols = 2;
+% initialize the information we store about the class averages
+erp      = zeros(sum(iseeg),trlen_samp,nSymbols);
+nTarget  = zeros(nSymbols,1);
+
+% make the figure window
+clf;
+hdls=image3d(erp,1,'plotPos',ch_pos,'xlabel','ch','Xvals',ch_names,'zlabel','class','disptype','plot','ticklabs','sw');
+
+
+% ----------------------------------------------------------------------------
+%    FILL IN YOUR CODE BELOW HERE
+% ----------------------------------------------------------------------------
+
+state=[];
+
+% block until we've got new events *and* data to process
+[data,devents,state]=buffer_waitData([],[],state,'startSet',{'stimulus.epoch'},'trlen_samp',trlen_samp,'exitSet',{'data' 'stimulus.sequences' 'end'});
+
+% loop over all matching events and data
+for ei=1:numel(devents)
+  % update the class averages
+  %...?
+  evt=devents(ei); % this events info
+  dat=data(ei);  % and associated data
+end
+
+% update the ERP plot
+hdls=image3d(erp,1,'handles',hdls,'xlabel','ch','Xvals',ch_names,'zlabel','class','disptype','plot','ticklabs','sw');
+drawnow;    
--- tutorial/lect3-helloworld/erpviewerStimulus_skel.m
+++ tutorial/lect3-helloworld/erpviewerStimulus_skel.m
@@ -1,7 +1,6 @@
-run ../../utilities/initPaths;
+run ../../utilities/initPaths.m;
 
 buffhost='localhost';buffport=1972;
-global ft_buff; ft_buff=struct('host',buffhost,'port',buffport);
 % wait for the buffer to return valid header information
 hdr=[];
 while ( isempty(hdr) || ~isstruct(hdr) || (hdr.nchans==0) ) % wait for the buffer to contain valid data
@@ -18,21 +17,42 @@ end;
 initgetwTime;
 initsleepSec;
 
+
+
 % ----------------------------------------------------------------------------
 %    FILL IN YOUR CODE BELOW HERE
 % ----------------------------------------------------------------------------
 
-% useful functions
+% useful constants
+interSeqDuration=3;
+interEpochDuration=1;
 
-% make a random sequence of nEpoch long with 2 classes
-[ans,ans,ans,stimCode]=mkStimSeqRand(2,nEpoch,[],0);
-
-
-% make the stimulus, i.e. a big circle in the middle of the axes
+% make the stimulus
 clf;
 set(gcf,'color',[0 0 0],'toolbar','none','menubar','none'); % black figure
 set(gca,'visible','off','color',[0 0 0]); % black axes
-h=rectangle('curvature',[1 1],'position',[.25 .25 .5 .5],'facecolor',[.5 .5 .5]);
+h=rectangle('curvature',[1 1],'position',[.25 .25 .5 .5],'facecolor',[.5 .5 .5]); % fixitation point
 set(h,'visible','off');
-% update the circles color
-set(h,'color',[1 1 1],'visible','on'); % make it white and visible
+
+% make the target sequence
+[ans,ans,ans,stimCode]=mkStimSeqRand(2,nEpoch,[],0);
+
+      
+% reset the cue and fixation point to indicate trial has finished  
+set(h,'visible','off'); 
+drawnow;
+  
+% make the cue visible again to indicate trial start
+set(h,'visible','on');
+drawnow;
+
+% change the cue's color as needed
+if ( epoch==1 ) 
+  set(h,'facecolor',[1 1 1]);
+else
+  set(h,'facecolor',[.5 .5 .5]);
+end
+drawnow;
+  
+% wait for a key press
+msg=msgbox({'Press OK to continue'},'Continue?');while ishandle(msg); pause(.2); end;
