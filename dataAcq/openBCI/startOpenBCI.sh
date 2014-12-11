de21d25d78b58d20916f515e0c4e44cb5c95b6ab
Jason Farquhar <jdrf@zepler.org>
2014-12-11 21:28:51 +0100

    neurofeedback updates + openBCI
    Former-commit-id: c4c3b6074d8fa91f2eab5f38583a8f60c10aba32

---
+++ dataAcq/openBCI/startOpenBCI.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+java -cp ../dataAcq/buffer/java/BufferClien.jar:.:jssc.jar openBCI2ft COM12
--- neurofeedback/configureNF.m
+++ neurofeedback/configureNF.m
@@ -1,37 +1,40 @@
 % guard to prevent running multiple times
-if ( exist('configRun','var') && ~isempty(configRun) ) return; end;
-configRun=true;
+if ( ~exist('configRun','var') || isempty(configRun) ) 
 
-run ../utilities/initPaths.m;
+  configRun=true;
 
-buffhost='localhost';buffport=1972;
-global ft_buff; ft_buff=struct('host',buffhost,'port',buffport);
-% wait for the buffer to return valid header information
-hdr=[];
-while ( isempty(hdr) || ~isstruct(hdr) || (hdr.nchans==0) ) % wait for the buffer to contain valid data
-  try 
-    hdr=buffer('get_hdr',[],buffhost,buffport); 
-  catch
-    hdr=[];
-    fprintf('Invalid header info... waiting.\n');
+  run ../utilities/initPaths.m;
+
+  buffhost='localhost';buffport=1972;
+  global ft_buff; ft_buff=struct('host',buffhost,'port',buffport);
+  % wait for the buffer to return valid header information
+  hdr=[];
+  while ( isempty(hdr) || ~isstruct(hdr) || (hdr.nchans==0) ) % wait for the buffer to contain valid data
+    try 
+      hdr=buffer('get_hdr',[],buffhost,buffport); 
+    catch
+      hdr=[];
+      fprintf('Invalid header info... waiting.\n');
+    end;
+    pause(1);
   end;
-  pause(1);
+
+  % set the real-time-clock to use
+  initgetwTime;
+  initsleepSec;
+
+  if ( exist('OCTAVE_VERSION','builtin') ) 
+    page_output_immediately(1); % prevent buffering output
+    if ( ~isempty(strmatch('qthandles',available_graphics_toolkits())) )
+      graphics_toolkit('qthandles'); % use fast rendering library
+    elseif ( ~isempty(strmatch('fltk',available_graphics_toolkits())) )
+      graphics_toolkit('fltk'); % use fast rendering library
+    end
+  end
+
 end;
 
-% set the real-time-clock to use
-initgetwTime;
-initsleepSec;
-
-if ( exist('OCTAVE_VERSION','builtin') ) 
-  page_output_immediately(1); % prevent buffering output
-  if ( ~isempty(strmatch('qthandles',available_graphics_toolkits())) )
-    graphics_toolkit('qthandles'); % use fast rendering library
-  elseif ( ~isempty(strmatch('fltk',available_graphics_toolkits())) )
-    graphics_toolkit('fltk'); % use fast rendering library
-  end
-end
-
-verb=2;
+verb=1;
 buffhost='localhost';
 buffport=1972;
 nSymbs=2; % number of points to move towards, need this many classifier predicitons
@@ -57,17 +60,28 @@ step_ms =100; % how often to compute the average output, 100ms=10x / s
 %  electrodes = specifies the set of electrodes to use. One of:
 %     [] - empty means average over all electrodes
 %     [nElect x 1] - gives a weighting over electrodes
+%     [nAve x 1]  - indices of the electrodes to average togeher. (use -index to negate)
 %     {'str'} - gives a list of electrode names to sum together
 %               N.B. use -Cz to negate before summing, 
 %                  e.g. to feedback on the lateralisation between C3 and C4 use: {'C3' '-C4'}
-% 1) frontal alpha lateralisation
+% 1) alpha power in electrode 1
 feedback = struct('label','alphaLat',...
                   'freqband',[8 12],...
-                  'electrodes',{{'FP1' '-FP2'}}); % don't forget double cell for struct
-% 2) EMG (= high freq power) + eye (=low freq power)
+                  'electrodes',2);%{{'FP1' '-FP2'}}); 
+% 2) alpha power in electrode 2
 feedback(2) = struct('label','badness',...
-                     'freqband',mkFilter(freqs,[0 0 4 4])*10 + mkFilter(freqs,[15 45]),...
-                     'electrodes',{{'FP1' 'FP2'}}); 
+                     'freqband',[8 12],...
+                     'electrodes',3);
+
+% % 2) EMG (= high freq power) + eye (=low freq power)
+% feedback(2) = struct('label','badness',...
+%                      'freqband',mkFilter(freqs,[0 0 4 4])*10 + mkFilter(freqs,[15 45]),...
+%                      'electrodes',2);%{{'FP1' 'FP2'}}); % don't forget double cell for struct
 
 % set smoothing rate for the estimated spectral powers
-expSmoothFactor = [exp(log(.5)/10000) exp(log(.5)/10)];%log(2)/log(10); %exp(log(.5)/10)
+% N.B. 0=no-smoothing, 1=infinite-smoothing
+% expSmoothFactor(1) =  smoothing on the standardisation-filter
+% expSmoothFactor(2) =  smoothing on the input features
+expSmoothFactor = [exp(log(.5)/1000) exp(log(.5)/10)];
+% additional smoothing for the stimulus
+stimulusSmoothFactor = exp(log(.5)./10); % additional smoothing for the cursor movement
