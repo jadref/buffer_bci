24e2f2fd5cf64bf21cf1390ffa9c4aeb5ed2617c
Jason Farquhar <jdrf@zepler.org>
2015-01-27 12:24:50 +0100

    lower used java version
    Former-commit-id: 4c45a26857a8d029c8247b91eb5c3bf5c246a35d

--- java/echoClient/compile.sh
+++ java/echoClient/compile.sh
@@ -1,11 +1,12 @@
+echo Note: limiting java version for max compatability with JRE's
 echo Building: javaclient
-javac -classpath ../../dataAcq/buffer/java/BufferClient.jar javaclient.java
+javac -target 1.4 -source 1.4 -classpath ../../dataAcq/buffer/java/BufferClient.jar javaclient.java
 echo Building: eventViewer
-javac -classpath ../../dataAcq/buffer/java/BufferClient.jar eventViewer.java
+javac -target 1.4 -source 1.4 -classpath ../../dataAcq/buffer/java/BufferClient.jar eventViewer.java
 cp eventViewer.class ../../dataAcq/buffer/java
 echo Building: syncGenerator
-javac -classpath ../../dataAcq/buffer/java/BufferClient.jar syncGenerator.java
+javac -target 1.5 -source 1.5 -classpath ../../dataAcq/buffer/java/BufferClient.jar syncGenerator.java
 echo Building: filePlayback
 cp filePlayback.class ../../dataAcq/buffer/java
-javac -classpath ../../dataAcq/buffer/java/BufferClient.jar filePlayback.java
+javac -target 1.5 -source 1.5 -classpath ../../dataAcq/buffer/java/BufferClient.jar filePlayback.java
 cp syncGenerator.class ../../dataAcq/buffer/java
