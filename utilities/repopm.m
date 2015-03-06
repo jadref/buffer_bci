bdfcd43bcbd33a4c1f2efb6f355ff32ce6ad58fc
Jason Farquhar <jdrf@zepler.org>
2015-03-06 10:22:31 +0100

    don't use bsxfn on octave as it's r e a l l y   s l o w
    Former-commit-id: f98437e6c5ce8af9afa48ee178e4e9524a33b0e9

--- utilities/repopm.m
+++ utilities/repopm.m
@@ -82,7 +82,9 @@ if ( isstr(op) )
 end;
 
 % use the matlab based builtin equivalent if available
-if ( exist('bsxfun','builtin') ) % matlab R2008 or later
+if ( numel(A)==1 || numel(B)==1 ) % scalar case
+  Z=feval(op,A,B);
+elseif ( exist('bsxfun','builtin') && ~exist('OCTAVE_VERSION','builtin') ) % matlab R2008 or later
   Z=bsxfun(op,A,B);
 else % older matlabs
   szA=size(A);
