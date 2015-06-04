e8f4c64c43c0f091f0a810847ad951feb6fe1725
Jason Farquhar <jdrf@zepler.org>
2015-06-04 15:28:34 +0200

    matlab based batch files startup improvements
    Former-commit-id: 16ea1556ee67ad3bb34217ff1045ab02facdc419

--- dataAcq/startFileProxy.bat
+++ dataAcq/startFileProxy.bat
@@ -1,4 +1,4 @@
 call ..\utilities\findMatlab.bat
 set batdir=%~dp0
 cd %batdir%
-start "Matlab" %matexe% -nodesktop -r "run ../utilities/initPaths.m;buffer_fileproxy([],[],'%1');quit;"
+echo run ../utilities/initPaths.m;buffer_fileproxy([],[],'%1');quit; | %matexe%
