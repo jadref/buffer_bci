6fbe43b261a1af6db91552ef8a7d0078ef3b8bbe
Jason Farquhar <jdrf@zepler.org>
2015-06-04 15:31:04 +0200

    added search for octave
    Former-commit-id: 146b6c7c25f2b5071061061718fd6ffef758cf2c

--- utilities/findMatlab.bat
+++ utilities/findMatlab.bat
@@ -1,9 +1,17 @@
 set matexe="" 
+rem search for octave first
+FOR /d %%f in ("C:\Program Files\Octave\*" "C:\Program Files (x86)\Octave\*" "C:\Octave\*") DO (
+	 if exist "%%f\bin\octave.exe" ( 
+	 set matexe="%%f\bin\octave.exe"
+rem	 exit /b 
+	 )
+)
+
+rem search matlab later, if found this is prefered
 FOR /d %%f in ("C:\Program Files\MATLAB\*" "C:\Program Files (x86)\MATLAB\*") DO (
 	 if exist "%%f\bin\matlab.exe" ( 
 	 set matexe="%%f\bin\matlab.exe"
 rem	 exit /b 
 	 )
 )
-
 
