de4b57489634c89cea77d4f2b689389e40493c64
Jason Farquhar <jdrf@zepler.org>
2015-06-04 23:58:08 +0200

    added help text for setup
    Former-commit-id: dddc507cc4393be90f2e6251d70135f7e4079a3d

--- dataAcq/startOpenBCI.bat
+++ dataAcq/startOpenBCI.bat
@@ -2,3 +2,7 @@ call ..\utilities\findJava.bat
 rem TODO : auto search for the serial device?
 set usbPort=COM1
 %javaexe% -cp "buffer/java/BufferClient.jar;openBCI/lib/jssc.jar;openBCI/openBCI2ft.jar" openBCI2ft %usbPort%
+if errorlevel 1 (
+	start cmd /c "echo If you cannot connect to the openBCI because the dongle doesnt connect.  Then you can try installing the dongle driver,from:\n buffer/win32/CDM\ v2.12.00\ WHQL\ Certified.exe? or download from : <http://www.ftdichip.com/Drivers/VCP.htm> && pause"
+)
+
