37192636c365d7808305ef60c234d21a53194c81
Jason Farquhar <jdrf@zepler.org>
2015-06-11 15:24:58 +0200

    stuff
    Former-commit-id: 199bdabf0e8858d536cb49db3efa525040dedee4

--- utilities/startSigViewer.bat
+++ utilities/startSigViewer.bat
@@ -1,7 +1,7 @@
 call ..\utilities\findMatlab.bat
 rem Argh matlab doesn't support stdin, octave doesn't support -r.....
 if "%ismatlab%" == 1 (
-  start "Matlab" /b %matexe% -r "sigViewer();quit()"
+  start "Matlab" /b %matexe% -r "sigViewer();quit()"  
 ) else (
   echo sigViewer();quit; | %matexe%
 )
