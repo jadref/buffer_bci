506a759d7e15901ad5909792ab32b21641d6aecd
Jason Farquhar <jdrf@zepler.org>
2015-06-11 15:35:43 +0200

    fix windows batch-file-startup
    Former-commit-id: 68181aaa0c068121a79729a33e13ee17304f9bc9

--- utilities/findMatlab.bat
+++ utilities/findMatlab.bat
@@ -1,5 +1,5 @@
 set matexe="" 
-set ismatlab=0
+set ismatlab=1
 FOR /d %%f in ("C:\Program Files\MATLAB\*" "C:\Program Files (x86)\MATLAB\*") DO (
 	 if exist "%%f\bin\matlab.exe" ( 
 	 set matexe="%%f\bin\matlab.exe"
--- utilities/startSigViewer.bat
+++ utilities/startSigViewer.bat
@@ -1,7 +1,6 @@
 call ..\utilities\findMatlab.bat
-rem Argh matlab doesn't support stdin, octave doesn't support -r.....
-if "%ismatlab%" == 1 (
+if %ismatlab%==1 (
   start "Matlab" /b %matexe% -r "sigViewer();quit()"  
 ) else (
-  echo sigViewer();quit; | %matexe%
+  echo "sigViewer();quit;" | %matexe%
 )
