85c0b010f9087211c572fa508eb53c139327f614
Jason Farquhar <jdrf@zepler.org>
2015-06-18 09:37:32 +0200

    startup scripts
    Former-commit-id: 2f10da069ffe063858345c6a83307275b9545280

--- dataAcq/startJavaEventViewer.sh
+++ dataAcq/startJavaEventViewer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 buffdir=`dirname $0`
-echo Starting: ${buffdir}/buffer/java/eventViewer.class $outdir $@
+echo Starting: ${buffdir}/buffer/java/eventViewer.class $@
 java -cp buffer/java/BufferClient.jar:buffer/java eventViewer $@
