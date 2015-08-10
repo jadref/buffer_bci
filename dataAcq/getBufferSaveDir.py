a4c21bf388bfad3cc9f3f0387947e03a2edad160
Jason Farquhar <jdrf@zepler.org>
2015-08-10 18:08:44 +0200

    make compatable with python2 and python3
    Former-commit-id: 3e4d92ed3d0b3f8f4e19da30a58e9da76886b2b0

--- dataAcq/getBufferSaveDir.py
+++ dataAcq/getBufferSaveDir.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python
-import Tkinter 
-#from Tkinter import ttk
-import  tkFileDialog
+# -*- coding: utf-8 -*-
+try:
+    # for Python2
+    from Tkinter import *   ## notice capitalized T in Tkinter 
+except ImportError:
+    # for Python3
+    from tkinter import *   ## notice here too
 import time
 import os
 
-root = Tkinter.Tk()
+root = Tk()
 root.title("Choose saving location")
 
-subject = Tkinter.StringVar()
+subject = StringVar()
 subject.set('test')
-experiment = Tkinter.StringVar()
+experiment = StringVar()
 experiment.set('expt')
-dataroot= Tkinter.StringVar()
+dataroot= StringVar()
 dataroot.set('~/output')
-saveloc = Tkinter.StringVar()
+saveloc = StringVar()
 saveloc.set('')
-session = Tkinter.StringVar()
+session = StringVar()
 session.set(os.path.normpath(time.strftime("%y%m%d/%H%M")))
 
 def updateSaveLoc(ignored=None):
@@ -28,28 +32,28 @@ def pickDir():
         dataroot.set(dnm)
         updateSaveLoc()
 
-mainframe = Tkinter.Frame(root)
-mainframe.grid(column=0, row=0, sticky=(Tkinter.N, Tkinter.W, Tkinter.E, Tkinter.S))
+mainframe = Frame(root)
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 
-Tkinter.Label(mainframe, text="Experiment :").grid(column=1, row=1, sticky=Tkinter.W)
-expt_entry = Tkinter.Entry(mainframe, width=30, textvariable=experiment)
-expt_entry.grid(column=2, row=1, sticky=Tkinter.W)
+Label(mainframe, text="Experiment :").grid(column=1, row=1, sticky=W)
+expt_entry = Entry(mainframe, width=30, textvariable=experiment)
+expt_entry.grid(column=2, row=1, sticky=W)
 expt_entry.bind("<Return>",updateSaveLoc)
 
-Tkinter.Label(mainframe, text="Subject :").grid(column=1, row=2, sticky=Tkinter.W)
-subject_entry = Tkinter.Entry(mainframe, width=30, textvariable=subject)
-subject_entry.grid(column=2, row=2, sticky=Tkinter.W)
+Label(mainframe, text="Subject :").grid(column=1, row=2, sticky=W)
+subject_entry = Entry(mainframe, width=30, textvariable=subject)
+subject_entry.grid(column=2, row=2, sticky=W)
 subject_entry.bind("<Return>",updateSaveLoc)
 
-Tkinter.Label(mainframe, text="Data Root :").grid(column=1, row=3, sticky=Tkinter.W)
-Tkinter.Button(mainframe, text="browse", command=pickDir).grid(column=2, row=3, sticky=Tkinter.E)
-Tkinter.Label(mainframe, textvariable=dataroot).grid(column=1, columnspan=2, row=4, sticky=Tkinter.W)
+Label(mainframe, text="Data Root :").grid(column=1, row=3, sticky=W)
+Button(mainframe, text="browse", command=pickDir).grid(column=2, row=3, sticky=E)
+Label(mainframe, textvariable=dataroot).grid(column=1, columnspan=2, row=4, sticky=W)
 
-Tkinter.Label(mainframe, text="Save location :").grid(column=1, row=5, sticky=Tkinter.W)
-Tkinter.Label(mainframe, textvariable=saveloc).grid(column=1, columnspan=2, row=6, sticky=Tkinter.W)
+Label(mainframe, text="Save location :").grid(column=1, row=5, sticky=W)
+Label(mainframe, textvariable=saveloc).grid(column=1, columnspan=2, row=6, sticky=W)
 
-Tkinter.Button(mainframe, text="Cancel", command=quit).grid(column=1, row=7, sticky=Tkinter.W)
-Tkinter.Button(mainframe, text="OK", command=mainframe.quit).grid(column=2, row=7, sticky=Tkinter.E)
+Button(mainframe, text="Cancel", command=quit).grid(column=1, row=7, sticky=W)
+Button(mainframe, text="OK", command=mainframe.quit).grid(column=2, row=7, sticky=E)
 
 subject_entry.focus()
 updateSaveLoc()
@@ -59,4 +63,4 @@ updateSaveLoc()
 outpath=os.path.expanduser(saveloc.get())
 if not os.path.exists(outpath):
     os.makedirs(outpath)
-print outpath
+print(outpath)
