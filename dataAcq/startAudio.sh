4ffb4fe95125a329d6fdab68cf3bd0fa49f61b54
Jason Farquhar <jdrf@zepler.org>
2015-08-28 23:42:11 +0200

    audio buffer startup script
    Former-commit-id: 1dac3f13b07983d2dd02e4ecb8c819dced86cdec

---
+++ dataAcq/startAudio.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env bash
+cd `dirname ${BASH_SOURCE[0]}`
+buffdir=`dirname $0`
+execname='audio2ft'
+if [ `uname -s` == 'Linux' ]; then
+	 if  [ "`uname -a`" == 'armv6l' ]; then
+		  arch='raspberrypi'
+    else
+		  arch='glnx86';
+   fi
+else # Mac
+	 arch='maci'
+fi
+buffexe="$buffdir/buffer/bin/${execname}";
+if [ -r $buffdir/${execname} ]; then
+    buffexe="$buffdir/${execname}";
+fi
+if [ -r $buffdir/buffer/bin/${arch}/${execname} ]; then
+	 buffexe="$buffdir/buffer/bin/${arch}/${execname}";
+fi
+if [ -r $buffdir/buffer/${arch}/${execname} ]; then
+	 buffexe="$buffdir/buffer/${arch}/${execname}";
+fi
+$buffexe 0 localhost 1972
