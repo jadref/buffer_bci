b68b7e38694de87a50bdbb5c5b5871e3abd7e754
Jason Farquhar <jdrf@zepler.org>
2015-09-03 10:35:03 +0200

    made the one-time init code
    Former-commit-id: 3c8556a84568e1f236814cad2c2a8561e8266da6

--- imaginedMovement/configureIM.m
+++ imaginedMovement/configureIM.m
@@ -1,35 +1,44 @@
-% guard to prevent running multiple times
-if ( exist('imConfig','var') && ~isempty(imConfig) ) return; end;
-imConfig=true;
+%----------------------------------------------------------------------
+% One-Time initialization code
+% guard to not run the slow one-time-only config code every time...
+if ( ~exist('configRun','var') || isempty(configRun) ) 
 
-run ../utilities/initPaths.m;
+  % setup the paths
+  run ../../utilities/initPaths.m;
 
-buffhost='localhost';buffport=1972;
-% wait for the buffer to return valid header information
-hdr=[];
-while ( isempty(hdr) || ~isstruct(hdr) || (hdr.nchans==0) ) % wait for the buffer to contain valid data
-  try 
-    hdr=buffer('get_hdr',[],buffhost,buffport); 
-  catch
-    hdr=[];
-    fprintf('Invalid header info... waiting.\n');
+  buffhost='localhost';buffport=1972;
+  % wait for the buffer to return valid header information
+  hdr=[];
+  while ( isempty(hdr) || ~isstruct(hdr) || (hdr.nchans==0) ) % wait for the buffer to contain valid data
+	 try 
+		hdr=buffer('get_hdr',[],buffhost,buffport); 
+	 catch
+		hdr=[];
+		fprintf('Invalid header info... waiting.\n');
+	 end;
+	 pause(1);
   end;
-  pause(1);
-end;
 
-% set the real-time-clock to use
-initgetwTime;
-initsleepSec;
+  % set the real-time-clock to use
+  initgetwTime;
+  initsleepSec;
 
-if ( exist('OCTAVE_VERSION','builtin') ) 
-  page_output_immediately(1); % prevent buffering output
-  if ( ~isempty(strmatch('qthandles',available_graphics_toolkits())) )
-    graphics_toolkit('qthandles'); % use fast rendering library
-  elseif ( ~isempty(strmatch('fltk',available_graphics_toolkits())) )
-    graphics_toolkit('fltk'); % use fast rendering library
+  if ( exist('OCTAVE_VERSION','builtin') ) 
+	 page_output_immediately(1); % prevent buffering output
+	 if ( ~isempty(strmatch('qthandles',available_graphics_toolkits())) )
+		graphics_toolkit('qthandles'); % use fast rendering library
+	 elseif ( ~isempty(strmatch('fltk',available_graphics_toolkits())) )
+		graphics_toolkit('fltk'); % use fast rendering library
+	 end
   end
-end
 
+  % One-time configuration has successfully completed
+  configRun=true;
+end
+%----------------------------------------------------------------------
+
+%----------------------------------------------------------------------
+% Application specific config
 verb=1;
 buffhost='localhost';
 buffport=1972;
