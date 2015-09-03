cd334a63e45a39ac1474b8ca4cb5e96ffd657bbb
Jason Farquhar <jdrf@zepler.org>
2015-09-03 14:47:05 +0200

    help text
    Former-commit-id: 9d65dd4c926048af735e7f3555363aa20f651db9

--- matlab/imageSaliance/loadnSliceImages.m
+++ matlab/imageSaliance/loadnSliceImages.m
@@ -16,7 +16,7 @@ for fi=1:numel(files);
 		  [ans,imageDB(nTargets).name] = fileparts(filei.name);
 		  imageDB(nTargets).image      = img;
 		  % cut into pieces and store the bits
-		  h=3; w=3; if ( size(img,2)>size(img,1)*1.5 ) w=4; end;
+		  h=3; w=3; if ( size(img,2)>size(img,1)*1.5 ) w=4; end; % decide for 3x3 or 3x4 pieces
 		  ys = round(linspace(1,size(img,1),h+1));
 		  xs = round(linspace(1,size(img,2),w+1));
 		  for i=1:w;
