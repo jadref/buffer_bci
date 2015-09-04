c1127b53ebcb8f98148318798a6c46002cd92e48
Jason Farquhar <jdrf@zepler.org>
2015-09-05 00:15:11 +0200

    java sig proc improvements
    Former-commit-id: 475f197cc722e6f20c29e70f6ff2102d5eed1a08

---
+++ java/sigProc/startContClassifier.sh
@@ -0,0 +1,1 @@
+java -cp "lib/BufferClient.jar:lib/commons-math3-3.4.jar:ContinuousClassifier.jar" nl.dcc.buffer_bci.signalprocessing.ContinuousClassifier localhost 1972 1000 res/sigproxy_alphaW.csv
