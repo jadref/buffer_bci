47b7da518e5e01fca146b47513ba6d8924ceec21
Jason Farquhar <jdrf@zepler.org>
2015-09-16 16:30:28 +0200

    maxEvents fixes
    Former-commit-id: 0f3fcfd21978c65d1e5c7f3f30bd219a4e0bfde4

--- python/audioOddball/startSigProcBuffer.bat
+++ python/audioOddball/startSigProcBuffer.bat
@@ -1,6 +1,6 @@
 call ..\..\utilities\findMatlab.bat
 if %ismatlab%==1 (
-  start "Matlab" /b %matexe% -r "run ../../utilities/initPaths.m;startSigProcBuffer('epochEventType','stimulus.target','freqband',[.1 .3 20 25],'clsfr_type','erp','trlen_ms',1000);" %matopts%
+  start "Matlab" /b %matexe% -r "run ../../utilities/initPaths.m;startSigProcBuffer('epochEventType','stimulus.target','freqband',[.1 .3 20 25],'clsfr_type','erp','trlen_ms',1000,'maxEvents',30*3);" %matopts%
 ) else (
-  echo run ../../utilities/initPaths.m;startSigProcBuffer('epochEventType','stimulus.target','freqband',[.1 .3 20 25],'clsfr_type','erp','trlen_ms',1000); | %matexe% %matopts%
+  echo run ../../utilities/initPaths.m;startSigProcBuffer('epochEventType','stimulus.target','freqband',[.1 .3 20 25],'clsfr_type','erp','trlen_ms',1000,'maxEvents',30*3); | %matexe% %matopts%
 )
