333c8240c533b1d3942d333eed40ded20d5319d4
Jason Farquhar <jdrf@zepler.org>
2015-09-16 23:34:53 +0200

    removed undeeded erpViewer startups
    Former-commit-id: 77e5dca21b35d967b5212f24dfc25991dc4ad253

--- matlab/imageSalience/startERPViewer.bat
+++
@@ -1,6 +0,0 @@
-call ..\utilities\findMatlab.bat
-if %ismatlab%==1 (
-  start "Matlab" /b %matexe% -r "../utilities/initPaths.m;erpViewer([],[],'trlen_ms',1000,'cuePrefix','stimulus.target','endType',{'startPhase.cmd' 'exit'});quit;" %matopts%
-) else (
-  echo run ../utilities/initPaths.m;erpViewer([],[],'trlen_ms',1000,'cuePrefix','stimulus.target','endType',{{'startPhase.cmd'} {'exit'}});quit; | %matexe% %matopts%
-)
