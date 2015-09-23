ac400f18b1cf6565329c3292d99b0cdf4962b829
Jason Farquhar <jdrf@zepler.org>
2015-09-23 16:55:43 +0200

    classpath update
    Former-commit-id: 503a4c0551f3c4f6bf37dea901377627625ffa2b

--- dataAcq/startJavaEventViewer.bat
+++ dataAcq/startJavaEventViewer.bat
@@ -1,5 +1,4 @@
 call ..\utilities\findJava.bat
 set batdir=%~dp0
-
 echo Starting: %javaexe% %batdir%/buffer/java/eventViewer.class
 %javaexe% -cp "%batdir%/buffer/java/BufferClient.jar;%batdir%/buffer/java" eventViewer
