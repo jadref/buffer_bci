62f22f22e3043d78a1537f607486ce92837e4597
Jason Farquhar <jdrf@zepler.org>
2015-09-28 16:06:25 +0200

    tidy up java fileplayback stuff
    Former-commit-id: f98acd702587a4502cc6aaf4a88b396621955d34

--- java/echoClient/compile.sh
+++ java/echoClient/compile.sh
@@ -1,4 +1,4 @@
-echo Note: limiting java version for max compatability with JRE's
+echo Note: limiting java version for max compatability with JREs
 echo Building: javaclient
 javac -target 1.4 -source 1.4 -classpath ../../dataAcq/buffer/java/BufferClient.jar javaclient.java
 echo Building: eventViewer
