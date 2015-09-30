fd21a0c3ab538fa924f0971f1663072ba663f4f7
Jason Farquhar <jdrf@zepler.org>
2015-09-30 16:06:35 +0200

    updated java startup scripts
    Former-commit-id: 20e139c219a21ebd856ae898575a274aecde4efd

---
+++ dataAcq/startMatlabEventViewer.bat
@@ -0,0 +1,8 @@
+call ..\utilities\findMatlab.bat
+set batdir=%~dp0
+cd %batdir%
+if %ismatlab%==1 (
+  start "Matlab" /b %matexe% -r "../utilities/initPaths.m;eventViewer();quit;" %matopts%
+) else (
+  echo run ../utilities/initPaths.m;eventViewer;quit; | %matexe% %matopts%
+)
