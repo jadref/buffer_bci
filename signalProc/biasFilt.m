fa39bf9b0a3f48fd924b4448eb1c8ef8f1c1dd07
Jason Farquhar <jdrf@zepler.org>
2015-10-01 17:09:08 +0200

    added warmup phase to the filter
    Former-commit-id: 2ee5e9a19933898468e15d2512b4f58e81223189

--- signalProc/biasFilt.m
+++ signalProc/biasFilt.m
@@ -1,7 +1,7 @@
 function [x,s]=biasFilt(x,s,alpha)
 % bias removing filter (high-pass) removes slow drifts from inputs
 %
-%   [x,s,mu,std]=stdFilt(x,s,alpha)
+%   [x,s]=biasFilt(x,s,alpha)
 %
 % Inputs:
 %   x - [nd x 1] the data to filter
@@ -14,18 +14,28 @@ function [x,s]=biasFilt(x,s,alpha)
 % Outputs:
 %   x - [nd x 1] filtered data,  x(t) = x(t) - fx(t)
 %   s - [struct] updated filter state
-if ( isempty(s) ) s=x; end;
+if ( isempty(s) ) s=struct('x',x,'n',1); end;
 if(any(alpha>1)) alpha=exp(log(.5)./alpha); end; % convert to decay factor
-s=alpha(:).*s + (1-alpha(:)).*x; % exp-decay moving average
-fprintf('x=[%s]\ts=[%s]',sprintf('%g ',x),sprintf('%g ',s));
-x=x-s;
-fprintf(' => x_new=[%s]\n',sprintf('%g ',x));
+if ( isstruct(s) ) % during warmup phase
+   alphaN=1-1/s.n; s.n=s.n+1; % alpha adapts w.r.t. the number of examples processed
+   s.x=alphaN(:).*s.x + (1-alphaN(:)).*x;
+   fprintf('x=[%s]\ts=[%s]',sprintf('%g ',x),sprintf('%g ',s.x));
+   x=x-s.x;
+   fprintf(' => x_new=[%s]\n',sprintf('%g ',x));
+   if ( alphaN > alpha ) s=s.x; fprintf('\n**end-warmup**\n'); end % just store the MA at the end of the warmup phase
+else
+   s=alpha(:).*s + (1-alpha(:)).*x; % exp-decay moving average
+   fprintf('x=[%s]\ts=[%s]',sprintf('%g ',x),sprintf('%g ',s));
+   x=x-s;
+   fprintf(' => x_new=[%s]\n',sprintf('%g ',x));
+end;
 return;
 function testCase()
-x=cumsum(randn(2,1000),2);
+x=cumsum(randn(2,200),2)+100;
+x(:,1)=x(:,1)+50;
 
 % simple test
-s=[];for i=1:size(x,2); [fx(:,i),si]=biasFilt(x(:,i),s,exp(log(.5)/100)); fs(:,i)=si; s=si; end;
+s=[];for i=1:size(x,2); [fx(:,i),si]=biasFilt(x(:,i),s,exp(log(.5)/100)); s=si; end;
 % feature specific smoothers
 s=[];for i=1:size(x,2); [fx(:,i),si]=biasFilt(x(:,i),s,[exp(log(.5)/100);exp(log(.5)/1000)]); fs(:,i)=si; s=si; end;
 
