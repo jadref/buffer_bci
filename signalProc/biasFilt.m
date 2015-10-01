3417b78b304a88efac59ffef809238c28eb11fed
Jason Farquhar <jdrf@zepler.org>
2015-10-01 18:47:54 +0200

    improved filter and startup
    Former-commit-id: 3c2d0ce723c0d3d089fd4127c9ac750d77862b4d

--- signalProc/biasFilt.m
+++ signalProc/biasFilt.m
@@ -1,7 +1,7 @@
-function [x,s]=biasFilt(x,s,alpha)
+function [x,s]=biasFilt(x,s,alpha,verb)
 % bias removing filter (high-pass) removes slow drifts from inputs
 %
-%   [x,s]=biasFilt(x,s,alpha)
+%   [x,s]=biasFilt(x,s,alpha,verb)
 %
 % Inputs:
 %   x - [nd x 1] the data to filter
@@ -13,30 +13,28 @@ function [x,s]=biasFilt(x,s,alpha)
 %           N.B. alpha = exp(log(.5)./(half-life))
 % Outputs:
 %   x - [nd x 1] filtered data,  x(t) = x(t) - fx(t)
-%   s - [struct] updated filter state
-if ( isempty(s) ) s=struct('x',x,'n',1); end;
+%   s - [struct] updated filter state, s.N = total weight, s.sx = smoothed estimate of x
+if ( nargin<3 || isempty(verb) ) verb=0; end;
+if ( isempty(s) ) s=struct('sx',zeros(size(x)),'N',0); end;
 if(any(alpha>1)) alpha=exp(log(.5)./alpha); end; % convert to decay factor
-if ( isstruct(s) ) % during warmup phase
-   alphaN=1-1/s.n; s.n=s.n+1; % alpha adapts w.r.t. the number of examples processed
-   s.x=alphaN(:).*s.x + (1-alphaN(:)).*x;
-   fprintf('x=[%s]\ts=[%s]',sprintf('%g ',x),sprintf('%g ',s.x));
-   x=x-s.x;
-   fprintf(' => x_new=[%s]\n',sprintf('%g ',x));
-   if ( alphaN > alpha ) s=s.x; fprintf('\n**end-warmup**\n'); end % just store the MA at the end of the warmup phase
-else
-   s=alpha(:).*s + (1-alpha(:)).*x; % exp-decay moving average
-   fprintf('x=[%s]\ts=[%s]',sprintf('%g ',x),sprintf('%g ',s));
-   x=x-s;
-   fprintf(' => x_new=[%s]\n',sprintf('%g ',x));
-end;
+s.N =alpha(:).*s.N  + (1-alpha(:)).*1; % weight accumulated so far, for adaptive weighting
+s.sx=alpha(:).*s.sx + (1-alpha(:)).*x;
+if ( verb>0 ) fprintf('x=[%s]\ts=[%s]',sprintf('%g ',x),sprintf('%g ',s.sx./s.N)); end;
+x=x-s.sx./s.N;
+if ( verb>0 ) fprintf(' => x_new=[%s]\n',sprintf('%g ',x)); end;
 return;
 function testCase()
 x=cumsum(randn(2,200),2)+100;
 x(:,1)=x(:,1)+50;
 
 % simple test
-s=[];for i=1:size(x,2); [fx(:,i),si]=biasFilt(x(:,i),s,exp(log(.5)/100)); s=si; end;
+s=[];for i=1:size(x,2); [fx(:,i),si]=biasFilt(x(:,i),s,exp(log(.5)/100)); s=si; fs(:,i)=si.sx; end;
 % feature specific smoothers
 s=[];for i=1:size(x,2); [fx(:,i),si]=biasFilt(x(:,i),s,[exp(log(.5)/100);exp(log(.5)/1000)]); fs(:,i)=si; s=si; end;
 
-clf;for si=1:size(x,1); subplot(size(x,1),1,si); plot([x(si,:);fx(si,:);fs(si,:)]');legend('x','filt(x)','bias');end;
+clf;
+si=1; idx=1:size(x,2);
+for si=1:size(x,1); 
+		subplot(size(x,1),1,si); 
+		plot([x(si,idx);fx(si,idx);fs(si,idx)]');legend('x','filt(x)','bias');
+end;
