024365afcb3f56faf5b37290dc736b216d3c2131
Jason Farquhar <jdrf@zepler.org>
2015-10-13 16:13:56 +0200

    work with greyscale input images
    Former-commit-id: 2886127951063a6ab8016a98724691891ff72bc6

--- matlab/imageSalience/loadImages.m
+++ matlab/imageSalience/loadImages.m
@@ -14,6 +14,7 @@ for fi=1:numel(files);
             continue;
         end
         [ans,imageDB(nTargets).name] = fileparts(filei.name);
+        if (size(img,3)==1) img=repmat(img,[1 1 3]); end
 		imageDB(nTargets).image      = img;
         nTargets=nTargets+1;
     end
