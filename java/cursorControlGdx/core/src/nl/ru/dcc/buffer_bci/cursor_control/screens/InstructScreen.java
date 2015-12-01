67de2f1cc4615412b923c9ce6b27a4ceee13eaf5
Lars Bokkers <LBokkers@kpnmail.nl>
2015-12-01 13:08:13 +0100

    Started with porting cursor control screens.
    Former-commit-id: 806b0c4a774f436941b5f22bef85e55749738ee0

---
+++ java/cursorControlGdx/core/src/nl/ru/dcc/buffer_bci/cursor_control/screens/InstructScreen.java
@@ -0,0 +1,22 @@
+package nl.ru.dcc.buffer_bci.cursor_control.screens;
+
+/**
+ * Created by Lars on 1-12-2015.
+ */
+public class InstructScreen extends CursorControlScreen {
+    private String instruction;
+
+    public void setInstruction(String instruction) {
+        this.instruction = instruction;
+    }
+
+    @Override
+    public void draw() {
+
+    }
+
+    @Override
+    public void update(float delta) {
+
+    }
+}
