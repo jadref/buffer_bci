2297ee3440486eccb0deb52485f318355685b389
Lars Bokkers <LBokkers@kpnmail.nl>
2015-12-01 23:54:54 +0100

    Added CursorScreen.
    Former-commit-id: 5f22fcb3831d5d32207ff53f0a68add9fa7fedaa

--- java/cursorControlGdx/desktop/src/nl/ru/dcc/buffer_bci/cursor_control/desktop/DesktopLauncher.java
+++ java/cursorControlGdx/desktop/src/nl/ru/dcc/buffer_bci/cursor_control/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package nl.ru.dcc.buffer_bci.cursor_control.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import nl.ru.dcc.buffer_bci.cursor_control.CursorControl;
+import nl.ru.dcc.buffer_bci.cursor_control.CursorControlGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new CursorControl(), config);
+		new LwjglApplication(new CursorControlGame(), config);
 	}
 }
