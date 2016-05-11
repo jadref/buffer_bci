7b428454775c3ea00dffc4961b84ab6dc67586ef
bcicourse <jadref@gmail.com>
2016-05-11 14:09:38 +0200

    run the event viewer in the foreground

--- dataAcq/startEventViewer.bat
+++ dataAcq/startEventViewer.bat
@@ -4,4 +4,5 @@ rem Search for the executable
 if exist "%batdir%buffer\bin\win32\eventViewer.exe" ( set buffexe="%batdir%buffer\bin\win32\eventViewer.exe" )
 if exist "%batdir%buffer\win32\eventViewer.exe" ( set buffexe="%batdir%buffer\win32\eventViewer.exe" )
 if exist "%batdir%eventViewer.exe" ( set buffexe="eventViewer.exe" )
-start /b "eventViewer" %buffexe% %*
+rem Run in foreground
+start "eventViewer" %buffexe% %*
