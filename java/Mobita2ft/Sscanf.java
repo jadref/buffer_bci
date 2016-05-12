76575fe8bb0f768e3d850e9ba0e0d63dfd91cf2b
Jason Farquhar <jdrf@zepler.org>
2016-05-12 15:34:48 +0200

    initial build of the java tmsi-driver

---
+++ java/Mobita2ft/Sscanf.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mobita2ft;
+
+/**
+ *
+ * @author H.G. van den Boorn
+ */
+// Code barrowed heavily from:
+// Copyright (c) 2003-2011, Jodd Team (jodd.org). All Rights Reserved.
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sscanf {
+
+    public static Object[] scan(String source, String format, Object... params) {
+        List<Object> outs = new ArrayList<Object>();
+        SscanfFormat sf = new SscanfFormat(source, format);
+
+        for (Object param : params) {
+            Object o = parse(sf, param);
+            if (o == null) {
+                break;
+            } else {
+                outs.add(o);
+            }
+        }
+        return outs.toArray();
+    }
+
+    public static int scan2(String source, String format, Object params[]) {
+        SscanfFormat sf = new SscanfFormat(source, format);
+        int parseCount = 0;
+
+        for (int i = 0; i < params.length; ++i) {
+            params[i] = parse(sf, params[i]);
+            if (params[i] == null) {
+                break;
+            } else {
+                ++parseCount;
+            }
+        }
+
+        return parseCount;
+    }
+
+    private static Object parse(SscanfFormat sf, Object param) {
+        if (!sf.prepareNextParseParam()) {
+            return null;
+        }
+        Object o = null;
+
+        if (param instanceof Number) {
+            if (param instanceof Integer) {
+                o = sf.parse((Integer) param);
+            } else if (param instanceof Long) {
+                o = sf.parse((Long) param);
+            } else if (param instanceof Double) {
+                //o = sf.parse((Double) param);
+            } else if (param instanceof Float) {
+                //o = sf.parse((Float) param);
+            } else {
+                //o = sf.parse((Number)param);
+            }
+        } else if (param instanceof Character) {
+            o = sf.parse((Character) param);
+        } else {
+            o = sf.parse(param.toString());
+        }
+
+        return o;
+    }
+}
---
+++ java/Mobita2ft/TMS_CONFIG_T.java
@@ -0,0 +1,26 @@
+package Mobita2ft;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+/** TMS config struct */
+public class TMS_CONFIG_T
+{
+  public short version; //*< PC Card protocol version number 0x0314
+  public short hdrSize; //*< size of measurement header 0x0200
+  public short fileType; //*< File Type (0: .ini 1: .smp 2:evt)
+  public int cfgSize; //*< size of config.ini  0x400
+  public short sampleRate; //*< File Type (0: .ini 1: .smp 2:evt)
+  public short nrOfChannels; //*< number of channels
+  public int startCtl; //*< start control
+  public int endCtl; //*< end control
+  public short cardStatus; //*< card status
+  public int initId; //*< Initialisation Identifier
+  public short sampleRateDiv; //*< Sample Rate Divider
+  public short mindecimation; //*< Minimum Decimantion of all channels
+//C++ TO JAVA CONVERTER TODO TASK: The typedef 'tms_storage_t' was defined in multiple preprocessor conditionals and cannot be replaced in-line:
+  public TMS_STORAGE_T[] storageType = new TMS_STORAGE_T[64]; //*< Storage Type
+  public Byte[] fileName = new Byte[12]; //*< Measurement file name
+  public LocalDateTime alarmTime = LocalDateTime.now(); //*< alarm time
+  public Byte[] info = new Byte[700]; //*< patient of measurement info
+}
