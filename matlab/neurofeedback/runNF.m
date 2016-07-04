662e09407768cf44542255422ac9ab92aba58b10
Jason Farquhar <jdrf@zepler.org>
2016-07-04 09:29:46 +0200

    octave support for neurofeedback controller

--- matlab/neurofeedback/runNF.m
+++ matlab/neurofeedback/runNF.m
@@ -1,35 +1,82 @@
 configureNF;
 % create the control window and execute the phase selection loop
-contFig=controller(); info=guidata(contFig); 
+if ~( exist('OCTAVE_VERSION','builtin') ) 
+  contFig=controller(); info=guidata(contFig); 
+else
+  contFig=figure(1);
+  set(contFig,'name','BCI Controller : close to quit','color',[0 0 0]);
+  axes('position',[0 0 1 1],'visible','off','xlim',[0 1],'ylim',[0 1],'nextplot','add');
+  set(contFig,'Units','pixel');wSize=get(contFig,'position');
+  fontSize = .05*wSize(4);
+  %        Instruct String          Phase-name
+  menustr={'0) EEG'                 'eegviewer';
+           '1) Practice'            'practice';
+			  '3) NeuroFeedback'       'neurofeedback'
+          };
+  txth=text(.25,.5,menustr(:,1),'fontunits','pixel','fontsize',.05*wSize(4),...
+				'HorizontalAlignment','left','color',[1 1 1]);
+  ph=plot(1,0,'k'); % BODGE: point to move around to update the plot to force key processing
+  % install listener for key-press mode change
+  set(contFig,'keypressfcn',@(src,ev) set(src,'userdata',char(ev.Character(:)))); 
+  set(contFig,'userdata',[]);
+  drawnow; % make sure the figure is visible
+end
+subject='test';
 while (ishandle(contFig))
   set(contFig,'visible','on');
-  uiwait(contFig); % CPU hog on ver 7.4
   if ( ~ishandle(contFig) ) break; end;
-  set(contFig,'visible','off');
-  info=guidata(contFig); 
-  subject=info.subject;
-  phaseToRun=lower(info.phaseToRun);
+
+  phaseToRun=[];
+  if ( ~exist('OCTAVE_VERSION','builtin') ) 
+	 uiwait(contFig);
+    if ( ~ishandle(contFig) ) break; end;    
+	 info=guidata(contFig); 
+	 subject=info.subject;
+	 phaseToRun=lower(info.phaseToRun);
+  else % give time to process the key presses
+	 % BODGE: move point to force key-processing
+	 fprintf('.');set(ph,'ydata',rand(1)*.01); drawnow;
+	 if ( ~ishandle(contFig) ) break; end;
+  end
+
+  % process any key-presses
+  modekey=get(contFig,'userdata'); 
+  if ( ~isempty(modekey) ) 	 
+	 fprintf('key=%s\n',modekey);
+	 phaseToRun=[];
+	 if ( ischar(modekey(1)) )
+		ri = strmatch(modekey(1),menustr(:,1)); % get the row in the instructions
+		if ( ~isempty(ri) ) 
+		  phaseToRun = menustr{ri,2};
+		elseif ( any(strcmp(modekey(1),{'q','Q'})) )
+		  break;
+		end
+	 end
+    set(contFig,'userdata',[]);
+  end
+
+  if ( isempty(phaseToRun) ) pause(.3); continue; end;
   fprintf('Start phase : %s\n',phaseToRun);
   
   switch phaseToRun;
     
    %---------------------------------------------------------------------------
    case 'capfitting';
-    sendEvent('subject',info.subject);
+    sendEvent('subject',subject);
     sendEvent('startPhase.cmd',phaseToRun);
     % wait until capFitting is done
     buffer_newevents(buffhost,buffport,[],phaseToRun,'end');
 
    %---------------------------------------------------------------------------
    case 'eegviewer';
-    sendEvent('subject',info.subject);
+    sendEvent('subject',subject);
     sendEvent('startPhase.cmd',phaseToRun);
     % wait until capFitting is done
     buffer_newevents(buffhost,buffport,[],phaseToRun,'end');
        
    %---------------------------------------------------------------------------
    case {'neurofeedback'};
-    sendEvent('subject',info.subject);
+    sendEvent('subject',subject);
     %sleepSec(.1);
     sendEvent(phaseToRun,'start');
     try
@@ -51,7 +98,7 @@ while (ishandle(contFig))
                            % re-place old info
     info.phasesCompleted=oinfo.phasesCompleted;
     info.phaseToRun=oinfo.phaseToRun;
-    info.subject=oinfo.subject; set(info.subjectName,'String',info.subject);
+    subject=osubject; set(subjectName,'String',subject);
     guidata(contFig,info);
   end;
 end
