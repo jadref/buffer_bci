d687a0477214153d42f84d31fbce1b3f59017735
Jason Farquhar <jdrf@zepler.org>
2016-07-25 20:51:05 +0200

    cybathalon specific version of imaginedMovement

---
+++ matlab/Cybathalon/configureIM.m
@@ -0,0 +1,101 @@
+%----------------------------------------------------------------------
+% One-Time initialization code
+% guard to not run the slow one-time-only config code every time...
+if ( ~exist('configRun','var') || isempty(configRun) ) 
+
+  % setup the paths
+  run ../utilities/initPaths.m;
+
+  buffhost='localhost';buffport=1972;
+  % wait for the buffer to return valid header information
+  hdr=[];
+  while( isempty(hdr) || ~isstruct(hdr) || (hdr.nchans==0) ) %wait for the buffer to contain valid data
+	 try 
+		hdr=buffer('get_hdr',[],buffhost,buffport); 
+	 catch
+		hdr=[];
+		fprintf('Invalid header info... waiting.\n');
+	 end;
+	 pause(1);
+  end;
+
+  % set the real-time-clock to use
+  initgetwTime;
+  initsleepSec;
+
+  if ( exist('OCTAVE_VERSION','builtin') ) 
+	 page_output_immediately(1); % prevent buffering output
+	 if ( ~isempty(strmatch('qt',available_graphics_toolkits())) )
+		graphics_toolkit('qt'); 
+	 elseif ( ~isempty(strmatch('qthandles',available_graphics_toolkits())) )
+		graphics_toolkit('qthandles'); 
+	 elseif ( ~isempty(strmatch('fltk',available_graphics_toolkits())) )
+		graphics_toolkit('fltk'); % use fast rendering library
+	 end
+  end
+
+  % One-time configuration has successfully completed
+  configRun=true;
+end
+%----------------------------------------------------------------------
+
+%----------------------------------------------------------------------
+% Application specific config
+verb         =1; % verbosity level for debug messages, 1=default, 0=quiet, 2=very verbose
+buffhost     ='localhost';
+buffport     =1972;
+nSymbs       =4; % E,N,W,S for 4 outputs, N,W,E  for 3 outputs
+symbCue      ={'RH' 'rst' 'LH' 'FT'}; % sybmol cue in addition to positional one. E,N,W,S for 4 symbs
+%nSymbs       =3;
+%symbCue      ={'rst' 'LH' 'RH'}; % string cue in addition to positional one. N,W,E for 3 symbs
+nSeq         =20*nSymbs; % 20 examples of each target
+
+trialDuration=3;
+baselineDuration=1;
+cueDuration  =1;
+startDelay   =.5;
+intertrialDuration=0;%3.5
+feedbackDuration=1;
+
+contFeedbackTrialDuration =10;
+neurofeedbackTrialDuration=30;
+warpCursor   = 0; % flag if in feedback BCI output sets cursor location or how the cursor moves
+moveScale    = .1;
+
+axLim        =[-1.5 1.5]; % size of the display axes
+winColor     =[0 0 0]; % window background color
+bgColor      =[.5 .5 .5]; % background/inactive stimuli color
+fixColor     =[1 0 0]; % fixitation/get-ready cue point color
+tgtColor     =[0 1 0]; % target color
+fbColor      =[0 0 1]; % feedback color
+
+% classifier training options
+trlen_ms      =trialDuration*1000; % how often to run the classifier
+welch_width_ms=250; % width of welch window => spectral resolution
+%trainOpts={'width_ms',welch_width_ms,'badtrrm',0}; % default: 4hz res, stack of independent one-vs-rest classifiers
+trainOpts={'width_ms',welch_width_ms,'badtrrm',0,'spatialfilter','wht','objFn','mlr_cg','binsp',0,'spMx','1vR'}; % whiten + direct multi-class training
+%trainOpts = {'spType',{{1 3} {2 4}}}; % train 2 classifiers, 1=N vs S, 2=E vs W
+
+% Epoch feedback opts
+%%0) Use exactly the same classification window for feedback as for training, but
+%%   but also include a bias adaption system to cope with train->test transfer
+epochFeedbackOpts={}; % raw output
+%epochFeedbackOpts={'predFilt',@(x,s) biasFilt(x,s,exp(log(.5)/50))}; % bias-apaption
+
+% different feedback configs (should all give similar results)
+
+%%1) Use exactly the same classification window for feedback as for training, but apply more often
+%contFeedbackOpts ={'step_ms',welch_width_ms}; % apply classifier more often
+%%   but also include a bias adaption system to cope with train->test transfer
+%contFeedbackOpts ={'predFilt',@(x,s) biasFilt(x,s,exp(log(.5)/100)),'step_ms',250};
+stimSmoothFactor= 0; % additional smoothing on the stimulus, not needed with 3s trlen
+
+%%2) Classify every welch-window-width (default 250ms), prediction is average of full trials worth of data, no-bias adaptation
+%% N.B. this is numerically identical to option 1) above, but computationally *much* cheaper 
+step_ms=welch_width_ms/2;% N.B. welch defaults=.5 window overlap, use step=width/2 to simulate
+contFeedbackOpts ={'predFilt',-(trlen_ms/step_ms),'trlen_ms',welch_width_ms};
+
+
+%%3) Classify every welch-window-width (default 500ms), with bias-adaptation
+%contFeedbackOpts ={'predFilt',@(x,s) biasFilt(x,s,exp(log(.5)/400)),'trlen_ms',[]}; 
+%stimSmoothFactor= -(trlen_ms/500);% actual prediction is average of trail-length worth of predictions
---
+++ matlab/Cybathalon/imCalibrateStimulus.m
@@ -0,0 +1,103 @@
+configureIM;
+
+% make the target sequence
+tgtSeq=mkStimSeqRand(nSymbs,nSeq);
+
+% make the stimulus
+%figure;
+fig=figure(2);
+set(fig,'Name','Imagined Movement','color',winColor,'menubar','none','toolbar','none','doublebuffer','on');
+clf;
+ax=axes('position',[0.025 0.025 .95 .95],'units','normalized','visible','off','box','off',...
+        'xtick',[],'xticklabelmode','manual','ytick',[],'yticklabelmode','manual',...
+        'color',winColor,'DrawMode','fast','nextplot','replacechildren',...
+        'xlim',axLim,'ylim',axLim,'Ydir','normal');
+
+stimPos=[]; h=[]; htxt=[];
+stimRadius=diff(axLim)/4;
+cursorSize=stimRadius/2;
+theta=linspace(0,2*pi,nSymbs+1);
+if ( mod(nSymbs,2)==1 ) theta=theta+pi/2; end; % ensure left-right symetric by making odd 0=up
+theta=theta(1:end-1);
+stimPos=[cos(theta);sin(theta)];
+for hi=1:nSymbs; 
+  h(hi)=rectangle('curvature',[1 1],'position',[stimPos(:,hi)-stimRadius/2;stimRadius*[1;1]],...
+                  'facecolor',bgColor); 
+  %if ( ~isempty(symbCue) ) % cue-text
+	% htxt(hi)=text(stimPos(1,hi),stimPos(2,hi),symbCue{hi},...
+	%					'HorizontalAlignment','center','color',[.1 .1 .1],'visible','on');
+  %end  
+end;
+% add symbol for the center of the screen
+stimPos(:,nSymbs+1)=[0 0];
+h(nSymbs+1)=rectangle('curvature',[1 1],'position',[stimPos(:,nSymbs+1)-cursorSize/2;cursorSize*[1;1]],...
+                      'facecolor',bgColor); 
+set(gca,'visible','off');
+
+%Create a text object with no text in it, center it, set font and color
+set(fig,'Units','pixel');wSize=get(fig,'position');set(fig,'units','normalized');% win size in pixels
+txthdl = text(mean(get(ax,'xlim')),mean(get(ax,'ylim')),' ',...
+				  'HorizontalAlignment', 'center', 'VerticalAlignment', 'middle',...
+				  'fontunits','pixel','fontsize',.05*wSize(4),...
+				  'color',[0.75 0.75 0.75],'visible','off');
+
+% play the stimulus
+% reset the cue and fixation point to indicate trial has finished  
+set(h(:),'facecolor',bgColor);
+sendEvent('stimulus.training','start');
+
+set(txthdl,'string', 'Click mouse when ready', 'visible', 'on'); drawnow;
+waitforbuttonpress;
+set(txthdl,'visible', 'off'); drawnow;
+
+for si=1:nSeq;
+
+  if ( ~ishandle(fig) ) break; end;
+
+  sleepSec(intertrialDuration);
+  % show the screen to alert the subject to trial start
+  set(h(end),'facecolor',fixColor); % red fixation indicates trial about to start/baseline
+  drawnow;% expose; % N.B. needs a full drawnow for some reason
+  sendEvent('stimulus.baseline','start');
+  sleepSec(baselineDuration);
+  sendEvent('stimulus.baseline','end');
+  
+  
+  % show the target
+  tgtIdx=find(tgtSeq(:,si)>0);
+  set(h(tgtSeq(:,si)>0),'facecolor',tgtColor);
+  set(h(tgtSeq(:,si)<=0),'facecolor',bgColor);
+  if ( ~isempty(symbCue) )
+	 set(txthdl,'string',sprintf('%s ',symbCue{tgtIdx}),'color',[.1 .1 .1],'visible','on');
+	 tgtNm = '';
+	 for ti=1:numel(tgtIdx);
+		if(ti>1) tgtNm=[tgtNm ' + ']; end;
+		tgtNm=sprintf('%s%d %s ',tgtNm,tgtIdx,symbCue{tgtIdx});
+	 end
+  else
+	 tgtNm = tgtIdx; % human-name is position number
+  end
+  set(h(end),'facecolor',[0 1 0]); % green fixation indicates trial running
+  fprintf('%d) tgt=%10s : ',si,tgtNm);
+  sendEvent('stimulus.target',tgtNm);
+  drawnow;% expose; % N.B. needs a full drawnow for some reason
+  sendEvent('stimulus.trial','start');
+  % wait for trial end
+  sleepSec(trialDuration);
+  
+  % reset the cue and fixation point to indicate trial has finished  
+  set(h(:),'facecolor',bgColor);
+  if ( ~isempty(symbCue) ) set(txthdl,'visible','off'); end
+  drawnow;
+  sendEvent('stimulus.trial','end');
+  
+  ftime=getwTime();
+  fprintf('\n');
+end % sequences
+% end training marker
+sendEvent('stimulus.training','end');
+
+if ( ishandle(fig) ) % thanks message
+set(txthdl,'string',{'That ends the training phase.','Thanks for your patience'}, 'visible', 'on', 'color',[0 1 0]);
+pause(3);
+end
