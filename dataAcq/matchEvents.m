a8bdc4f93ecc5aed678a8230f58dbeda5e2bc5e2
Jason Farquhar <jdrf@zepler.org>
2016-10-06 16:24:41 +0200

    mixed types over events matching

--- dataAcq/matchEvents.m
+++ dataAcq/matchEvents.m
@@ -16,43 +16,69 @@ if ( ischar(mval) && ~isequal(mval,'*') )   mval={mval}; end;
 % find matching types
 mi=true(size(ev));
 if ( isequal(mtype,'*') )
-elseif ( isnumeric(mtype) )
-  type=[ev.type]; type=type(:);
-  mi=any(repop(type(mi),'==',mtype(:)'),2);
-elseif ( iscell(mtype) && ischar(mtype{1}) )
-  mi(:)=false;
-  for ei=1:numel(mi);
-    estr=ev(ei).type;
-    for vi=1:numel(mtype);
-      mstr=mtype{vi};
-      if ( strcmp(estr,mstr) || ... % normal match || prefix match
-           ( mstr(end)=='*' && numel(estr)>=numel(mstr)-1 && strcmp(estr(1:numel(mstr)-1),mstr(1:end-1))) )
-        mi(ei)=true; break; 
-      end
-    end
-  end
 else
-  warning('Unrec type-match spec: ignored!');
+										  % extract all the type info
+  if ( ischar(ev(1).type) )
+	 type={ev.type}; type=type(:);
+  else
+	 try;   type  =cat(1,ev.type); % single type, matrix
+	 catch; type ={ev.type}; type=type(:); % mixed types => cell-array
+	 end
+  end
+  if ( isnumeric(mtype) && isnumeric(type) ) % fast path
+	 mi=any(repop(type(mi),'==',mtype(:)'),2);
+  elseif ( iscell(type) )
+	 mi(:)=false;
+	 for ei=1:numel(mi);
+		estr=type{ei};
+		for vi=1:numel(mtype);
+        if ( iscell(mtype) ) mstr=mtype{vi}; else mstr=mtype(vi); end;
+        if ( ischar(estr) && ischar(mstr) )
+			 if ( strcmp(estr,mstr) || ... % normal match || prefix match
+					( mstr(end)=='*' && numel(estr)>=numel(mstr)-1 && strcmp(estr(1:numel(mstr)-1),mstr(1:end-1))) )
+				mi(ei)=true; break;
+			 end
+		  elseif ( isequal(estr,mstr) )
+			 mi(ei)=true; break;
+		  end				
+		end
+	 end
+  else
+	 warning('1:Unrec type-match spec: ignored!');
+  end
 end
 % find matching values
 if ( isequal(mval,'*') )
-elseif ( isnumeric(mval) )
-  value=[ev.value]; value=value(:);
-  mi(mi)=any(repop(value(mi),'==',mval(:)'),2);
-elseif ( iscell(mval) && ischar(mval{1}) )
-  ms =find(mi);
-  mi(ms)=false;
-  for ei=ms(:)';
-    vstr=ev(ei).value;
-    for vi=1:numel(mval);
-      mstr=mval{vi};
-      if ( strcmp(vstr,mstr) || ... % normal match || prefix match
-           ( mstr(end)=='*' && numel(vstr)>=numel(mstr)-1 && strcmp(vstr(1:numel(mstr)-1),mstr(1:end-1))) ) 
-        mi(ei)=true; break; 
-      end;     
-    end
-  end
 else
-  warning('Unrec val-match spec: ignored!');
+														  % extract the values
+  if ( ischar(ev(1).value) )
+	 value={ev.value}; value=value(:);
+  else
+	 try;   value =cat(1,ev.value); % single type, matrix
+	 catch; value ={ev.value}; value=value(:); % mixed types => cell-array
+	 end
+  end
+  if ( isnumeric(mval) && isnumeric(value) )
+	 mi(mi)=any(repop(value(mi),'==',mval(:)'),2);
+  elseif ( iscell(type) )
+	 ms =find(mi);
+	 mi(ms)=false;
+	 for ei=ms(:)';
+		vstr=value{ei};
+		for vi=1:numel(mval);
+        if ( iscell(mval) ) mstr=mval{vi}; else mstr=mval(vi); end;
+		  if ( ischar(vstr) && ischar(mstr) )
+			 if ( strcmp(vstr,mstr) || ... % normal match || prefix match
+					( mstr(end)=='*' && numel(vstr)>=numel(mstr)-1 && strcmp(vstr(1:numel(mstr)-1),mstr(1:end-1))) ) 
+				mi(ei)=true; break; 
+			 end;
+		  elseif ( isequal(vstr,mstr) )
+			 mi(ei)=true; break;
+		  end
+		end
+	 end
+  else
+	 warning('2:Unrec val-match spec: ignored!');
+  end
 end
 return;
