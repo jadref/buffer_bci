1f1df95eefda879ea01ed902c318b4cf58a0e9e6
Jason Farquhar <jdrf@zepler.org>
2016-11-09 13:18:48 +0100

    bug fix and improved plotting

--- tutorial/lect2-sigProc/SigProcERP.m
+++ tutorial/lect2-sigProc/SigProcERP.m
@@ -1,8 +1,8 @@
-% initialise the matlab paths
+%% initialise the matlab paths
 try; cd(fileparts(mfilename('fullpath')));catch; end;
 run ../../matlab/utilities/initPaths.m
 
-% load the data this contains
+%% load the data this contains
 % X - [ channels x time x epochs ] raw EEG data
 % Y - [ epochs x 1 ] class labels for X's epochs
 % fs- [int] sample rate of X
@@ -12,37 +12,38 @@ load('ERPdata');
 times=(1:size(X,2))/fs;
 yvals=times;
 
-% plot the first 3 epochs of the data in a topo-graphic multi-plot
-figure(1);clf;image3d(X(:,:,1:3),1,'plotPos',Cpos,'Xvals',Cnames,'Yvals',yvals,'ylabel','time (s)','disptype','plot','ticklabs','sw');
-% plot the class averages
+%% plot the first 3 epochs of the data in a topo-graphic multi-plot
+figure(1);clf;image3d(X(:,:,1:3),1,'plotPos',Cpos,'Xvals',Cnames,'Yvals',yvals,'ylabel','time (s)','zlabel','epoch','clabel','uV','disptype','plot','ticklabs','sw');
+%% plot the class averages
 erp = cat(3,mean(X(:,:,Y>0),3),mean(X(:,:,Y<=0),3));
 figure(2);clf;image3d(erp,1,'plotPos',Cpos,'Xvals',Cnames,'Yvals',yvals,'Zvals',{'pos','neg'},'disptype','plot','ticklabs','sw');
 
 zoomplots; % allow interactive zooming of the plots to see better what's going on
 
 
-% 1) detrend and try again
+%% 1) detrend and try again
 fprintf('1) Detrend\n');
 X=detrend(X,2);
 
-% 2) remove bad channels
+%% 2) remove bad channels
 fprintf('2) bad channel removal, ');
 % compute and plot the power in each channel..
-chpow = sum(X(:,:).^2,2);
+chpow = sqrt(sum(X(:,:).^2,2)./size(X,2)./size(X,3)); % mean amplitude per channel
 figure(1); clf;plot(chpow);
-set(gca,'xtick',1:2:numel(Cnames),'xtickmark',Cnames(1:2:end)); % put on the channel names..
+set(gca,'xtick',1:2:numel(Cnames),'xticklabel',Cnames(1:2:end)); % put on the channel names..
 
-% automatically identify the bad channels
-badch = idOutliers(X,1,2.5);
+%% automatically identify the bad channels
+[badch,feat,threshs] = idOutliers(X,1,2.5);
 % show the bad channels on the power plot
-figure(1);hold on;plot(find(badch),zeros(size(find(badch))),'*','markersize',10); 
+figure(1);hold on;plot(find(badch),ones(size(find(badch)))*threshs(end),'r*','markersize',10); 
+
 % remove them from the data
 X=X(~badch,:,:);
 % don't forget to update the channel info too!
 Cpos=Cpos(:,~badch);
 Cnames=Cnames(~badch);
 
-% 3) re-reference - CAR
+%% 3) re-reference - CAR
 fprintf('3) CAR\n');
 % plot the common average signal, for all trials
 figure(1);clf; plot(shiftdim(mean(X,1)))
@@ -50,39 +51,39 @@ figure(1);clf; plot(shiftdim(mean(X,1)))
 X =X - repmat(mean(X,1),[size(X,1),1,1]);
 % alt faster version: X = repop(X,'-',mean(X,1));
 
-% 4) spectrally filter
+%% 4) spectrally filter
 fprintf('4) filter\n');
 filt=mkFilter(size(X,2)/2,[.1 .3 12 15],fs/size(X,2));
 % visualize this filter
-figure(1); plot((1:numel(filt))*fs/size(X,2),filt); 
+figure(1); clf;plot((1:numel(filt))*fs/size(X,2),filt);xlabel('freq (hz)'); 
 % apply the filter to the data
 X   =fftfilter(X,filt,[],2);
 
-% 4.5) remove bad epochs
+%% 4.5) remove bad epochs
 fprintf('4.5) bad trial removal');
 % plot the epoch/trial powers
-eppow = sum(1,reshape(X,[],size(X,3)).^2);
-figure(1); clf; plot(eppow);
+eppow = sqrt(sum(reshape(X,[],size(X,3)).^2,1)./size(X,1)./size(X,2));
+figure(1); clf; plot(eppow); xlabel('epoch'); ylabel('amplitude');
 % automatically identify the bad epochs
-badep = idOutliers(X,3,3);
+[badep,feat,threshs] = idOutliers(X,3,3);
 % show them one the power plot
-figure(1);hold on;plot(find(badep),zeros(size(find(badep))),'*','markersize',10); 
+figure(1);hold on;plot(find(badep),ones(size(find(badep)))*threshs(end),'r*','markersize',10); 
 
-% store the bad examples to plot them
+%% store the bad examples to plot them
 Xbad = X(:,:,badep); 
 % plot the bad epochs
-figure(1); clf;image3d(Xbad,1,'plotPos',Cpos,'Xvals',Cnames,'disptype','plot','ticklabs','sw');
+figure(1); clf;image3d(Xbad,1,'plotPos',Cpos,'Xvals',Cnames,'Yvals',yvals,'Zvals',find(badep),'disptype','plot','ticklabs','sw','ylabel','time (s)','zlabel','epoch','clabel','uV');
 
 % remove the bad epochs from the data
 X=X(:,:,~badep);
 Y=Y(~badep); % don't forget to update labels!
 
-%6) train classifier
+%%6) train classifier
 fprintf('6) train classifier\n');
 [clsfr, res]=cvtrainLinearClassifier(X,Y,[],10);
-% Plot the trained classifier weight vector (only for linear classifiers)
-clf;image3d(clsfr.W,1,'plotPos',Cpos,'Xvals',Cnames,'disptype','plot','ticklabs','sw'); % plot bad info
+%% Plot the trained classifier weight vector (only for linear classifiers)
+figure(1);clf;image3d(clsfr.W,1,'plotPos',Cpos,'Xvals',Cnames,'Yvals',yvals,'disptype','plot','ticklabs','sw','ylabel','time (s)','zlabel','clsfr','clabel','weight'); % plot bad info
 
 %---------------------------------------------------------------------------------------------------
-%7) use train_erp_clsfr which does all this for you!
+%%7) use train_erp_clsfr which does all this for you!
 [clsfr,res,X]=train_erp_clsfr(X,Y,'ch_pos',Cnames,'fs',fs);
--- tutorial/lect2-sigProc/SigProcERSP.m
+++ tutorial/lect2-sigProc/SigProcERSP.m
@@ -1,8 +1,8 @@
-% initialise the matlab paths
+%% initialise the matlab paths
 try; cd(fileparts(mfilename('fullpath')));catch; end;
 run ../../matlab/utilities/initPaths.m
 
-% load the data this contains
+%% load the data this contains
 % X - [ channels x time x epochs ] raw EEG data
 % Y - [ epochs x 1 ] class labels for X's epochs
 % fs- [int] sample rate of X
@@ -11,46 +11,69 @@ run ../../matlab/utilities/initPaths.m
 load('ERSPdata'); 
 times=(1:size(X,2))/fs; yvals=times;
 
-% plot the first 3 epochs of the data in a topo-graphic multi-plot
-clf;image3d(X(:,:,1:3),1,'plotPos',Cpos,'Xvals',Cnames,'Yvals',yvals,'ylabel','time (s)','disptype','plot','ticklabs','sw');
-% plot the class averages
+%% plot the first 3 epochs of the data in a topo-graphic multi-plot
+figure(1);clf;image3d(X(:,:,1:3),1,'plotPos',Cpos,'Xvals',Cnames,'Yvals',yvals,'ylabel','time (s)','disptype','plot','ticklabs','sw');
+%% plot the class averages
 erp = cat(3,mean(X(:,:,Y>0),3),mean(X(:,:,Y<=0),3));
-clf;image3d(erp,1,'plotPos',Cpos,'Xvals',Cnames,'Yvals',yvals,'ylabel','time (s)','Zvals',{'pos','neg'},'disptype','plot','ticklabs','sw');
+figure(2);clf;image3d(erp,1,'plotPos',Cpos,'Xvals',Cnames,'Yvals',yvals,'ylabel','time (s)','Zvals',{'pos','neg'},'disptype','plot','ticklabs','sw');
 
 zoomplots; % allow interactive zooming of the plots to see better what's going on
 
-% 1) detrend and try again
+%% 1) detrend and try again
 fprintf('1) Detrend\n');
 X=detrend(X,2);
 
-% 2) remove bad channels
+%% 2) remove bad channels
 fprintf('2) bad channel removal, ');
-badch = idOutliers(X,1,2.5);
+% compute and plot the power in each channel..
+chpow = sqrt(sum(X(:,:).^2,2)./size(X,2)./size(X,3)); % mean amplitude per channel
+figure(1); clf;plot(chpow);
+set(gca,'xtick',1:2:numel(Cnames),'xticklabel',Cnames(1:2:end)); % put on the channel names..
+
+%% automatically identify the bad channels
+[badch,feat,threshs] = idOutliers(X,1,2.5);
+% show the bad channels on the power plot
+figure(1);hold on;plot(find(badch),ones(size(find(badch)))*threshs(end),'r*','markersize',10); 
+
+% remove them from the data
 X=X(~badch,:,:);
 % don't forget to update the channel info too!
 Cpos=Cpos(:,~badch);
 Cnames=Cnames(~badch);
 
-% 3) re-reference - CAR
+%% 3) re-reference - CAR
 fprintf('3) CAR\n');
+% plot the common average signal, for all trials
+figure(1);clf; plot(shiftdim(mean(X,1)))
 X =X - repmat(mean(X,1),[size(X,1),1,1]);
 % alt faster version: X = repop(X,'-',mean(X,1));
 
-% 3.5) remove bad epochs
+%% 3.5) remove bad epochs
 fprintf('3.5) bad trial removal');
+% plot the epoch/trial powers
+eppow = sum(1,reshape(X,[],size(X,3)).^2);
+figure(1); clf; plot(eppow);
+% automatically identify the bad epochs
 badep = idOutliers(X,3,3);
+% show them one the power plot
+figure(1);hold on;plot(find(badep),zeros(size(find(badep))),'*','markersize',10); 
+
+%% store the bad examples and plot them
 Xbad = X(:,:,badep); % store bad example to plot it..
-% clf;image3d(Xbad,1,'plotPos',Cpos,'Xvals',Cnames,'disptype','plot','ticklabs','sw'); % plot bad info
+% plot the bad epochs
+figure(1); clf;image3d(Xbad,1,'plotPos',Cpos,'Xvals',Cnames,'Zvals',find(badep),'disptype','plot','ticklabs','sw','ylabel','samples','zlabel','epoch','clabel','uV');
+
+% remove the bad epochs from the data
 X=X(:,:,~badep);
 Y=Y(~badep); % don't forget to update labels!
 
-%4) welch to convert to power spectral density
+%%4) welch to convert to power spectral density
 fprintf('4) Welch\n');
 [X,wopts,winFn]=welchpsd(X,2,'width_ms',500,'fs',fs);
 freqs=0:(1000/500):fs; % position of the frequency bins
 yvals=freqs;
 
-%5) sub-select the range of frequencies we care about
+%%5) sub-select the range of frequencies we care about
 freqband = [8 28];
 fidx=[];
 fprintf('5) Select frequencies\n');
@@ -59,12 +82,14 @@ fprintf('5) Select frequencies\n');
 X=X(:,fidx(1):fidx(2),:); % sub-set to the interesting frequency range
 yvals=yvals(fidx(1):fidx(2)); % update so plots use correct info
 
-%6) train classifier
+%%6) train classifier
 fprintf('6) train classifier\n');
 [clsfr, res]=cvtrainLinearClassifier(X,Y,[],10);
-% Plot the trained classifier weight vector (only for linear classifiers)
-clf;image3d(clsfr.W,1,'plotPos',Cpos,'Xvals',Cnames,'disptype','plot','ticklabs','sw'); % plot bad info
+% show the classifier structure
+clsfr
+%% Plot the trained classifier weight vector (only for linear classifiers)
+figure(1);clf;image3d(clsfr.W,1,'plotPos',Cpos,'Xvals',Cnames,'Yvals',yvals,'disptype','plot','ticklabs','sw','ylabel','freq (hz)','zlabel','clsfr','clabel','weight'); % plot bad info
 
 %---------------------------------------------------------------------------------------------------
-%7) use train_ersp_clsfr which does all this for you!
+%%7) use train_ersp_clsfr which does all this for you!
 [clsfr,res]=train_ersp_clsfr(X,Y,'freqband',[8 28],'ch_pos',Cnames,'fs',fs);
