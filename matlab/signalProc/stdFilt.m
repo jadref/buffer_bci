538afd515ff239c886c6fbd5c8b554245bf470ec
Jason Farquhar <jdrf@zelper.org>
2017-05-29 18:47:44 +0200

    multiple decay factors

--- matlab/signalProc/stdFilt.m
+++ matlab/signalProc/stdFilt.m
@@ -10,27 +10,49 @@ function [x,s,mu,std]=stdFilt(x,s,alpha)
 %                   all [1x1] or each [ndx1] input feature
 %           fx(t) = (\sum_0^inf x(t-i)*alpha^i)/(\sum_0^inf alpha^i)
 %           fx(t) = (1-alpha) x(t) + alpha fx(t)
-%           N.B. alpha = exp(log(.5)./(half-life))
+%         OR
+%           [ndx2] 2 decay factors.  
+%                    alpha(1) = decay for mu/std estimation
+%                    alpha(2) = decay for averaging raw inputs (high-pass)
 % Outputs:
 %   x - [nd x 1] filtered data
 %   s - [struct] updated filter state
-if ( isempty(s) ) s=struct('N',0,'sx',zeros(size(x)),'sx2',zeros(size(x))); end;
-% mu/std statistics recording
-s.N  = alpha*s.N  + (1-alpha)*1;
-s.sx = alpha*s.sx + (1-alpha)*x; 
-s.sx2= alpha*s.sx2+ (1-alpha)*x.^2;
+if ( isempty(s) ) s=struct('N',zeros(size(x)),'sx',zeros(size(x)),'sx2',zeros(size(x)),'x',zeros(size(x))); end;
+if ( any(alpha>1) ) alpha=exp(log(.5)./alpha); end; % convert to decay factor
+s.N  = alpha.*s.N  + (1-alpha).*1;
+if ( size(alpha,2)>1 ) 
+  s.x= alpha(:,2).*s.x + (1-alpha(:,2)).*x; 
+  x  = s.x./s.N(:,2); 
+end;
+% moving average summary statistics for mu/std 
+s.sx = alpha(:,1).*s.sx + (1-alpha(:,1)).*x; 
+s.sx2= alpha(:,1).*s.sx2+ (1-alpha(:,1)).*x.^2;
 % current mean/variance
-mu=s.sx./s.N;
-std=sqrt(abs((s.sx2-s.sx.^2./s.N)./s.N));
+mu=s.sx./s.N(:,1);
+std=sqrt(abs((s.sx2-s.sx.^2./s.N(:,1))./s.N(:,1)));
 std(std<eps)=1; % deal with 0-variance channels
-if ( s.N>(1-alpha) ) x=(x-mu)./std; end;
+% standardize features for which we have enough weighting
+goodIdx=s.N(:,1)>(1-alpha(:,1));
+if( any(goodIdx) ) 
+  x(goodIdx)=(x(goodIdx)-mu(goodIdx))./std(goodIdx);
+end
 return;
-
 function testCase()
 x=cumsum(randn(2,10000),2);
 mu=zeros(size(x)); std=zeros(size(x)); fx=zeros(size(x)); 
+
+% simple stdandardizing filter
 s=[]; for i=1:size(x,2); [fx(:,i),s,mu(:,i),std(:,i)]=stdFilt(x(:,i),s,exp(log(.5)/100)); end;
+s=[]; for i=1:size(x,2); [fx(:,i),s,mu(:,i),std(:,i)]=stdFilt(x(:,i),s,100); end;
+
+% high-pass and stdandardise
+s=[]; for i=1:size(x,2); [fx(:,i),s,mu(:,i),std(:,i)]=stdFilt(x(:,i),s,[exp(log(.5)/100) exp(log(.5)/10)]); end;
+
+% test with different smoothers for different features
+s=[]; for i=1:size(x,2); [fx(:,i),s,mu(:,i),std(:,i)]=stdFilt(x(:,i),s,[exp(log(.5)/100) exp(log(.5)/10);exp(log(.5)/400) 1]); end;
+
 clf;
 for si=1:size(x,1); 
-  subplot(size(x,1),1,si); plot([x(si,:);fx(si,:);mu(si,:);mu(si,:)+std(si,:)]');legend('x','filt(x)','mu','std'); 
+  subplot(size(x,1),1,si); plot([x(si,:);fx(si,:)*10;mu(si,:);mu(si,:)+std(si,:)]');legend('x','filt(x)*10','mu','std'); 
 end;
+
