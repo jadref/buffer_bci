22fcca7cb156ec925e96dc69eafcb20e6e58aaad
Jason Farquhar <jadref@gmail.com>
2017-11-02 23:26:53 +0100

    working version

--- tutorial/lect5-visspell/spCalibrateStimulus_soln.py
+++ tutorial/lect5-visspell/spCalibrateStimulus_soln.py
@@ -19,9 +19,10 @@ DEBUG=False #True #
 ## HELPER FUNCTIONS
 def drawnow(fig=None):
     "force a matplotlib figure to redraw itself, inside a compute loop"
-    if fig is None: fig=plt.gcf()
+    if fig is None : fig=plt.gcf()
     fig.canvas.draw()
-    plt.pause(1e-3) # wait for draw.. 1ms
+    fig.canvas.flush_events()
+    #plt.pause(1e-3) # wait for draw.. 1ms
 
 currentKey=None
 def keypressFn(event):
@@ -38,7 +39,23 @@ def waitforkey(fig=None,reset=True,debug=DEBUG):
     while currentKey is None:
         plt.pause(1e-2) # allow gui event processing
 
+## CONFIGURABLE VARIABLES
+verb=0
+symbols=['a', 'b', 'c', 'd']
+nSymbs =sum([len(r) for r in symbols])
+nSeq=6
+nRep=5
+cueDuration=2
+postCueDuration=1
+epochDuration=0#.1
+interEpochDuration=0#.1
+interSeqDuration=2
 
+bgColor =(.5,.5,.5)
+tgtColor=(0,1,0)
+fixColor=(1,0,0)
+txtColor=(1,1,1)
+flashColor=(1,1,1)
 
 def initGrid(symbols,ax=None,txtColor=txtColor):
     "Initialize a grid of letters on the screen"
@@ -46,46 +63,33 @@ def initGrid(symbols,ax=None,txtColor=txtColor):
         ax=plt.gca()
     axw=ax.get_xlim()
     axh=ax.get_ylim()
-    h = (axw[1]-axw[0])/(len(symbols)+1)
-    w = (axh[1]-axh[0])/(max([len(r) for r in symbols]))
+    print('Symbs(%d)=[%s]'%(len(symbols),[str(s) for s in symbols]))
+    w = (axw[1]-axw[0])/(len(symbols)+1)
+    h = (axh[1]-axh[0])/(max([len(r) for r in symbols]))
+    print('xlim=[%f,%f]/%d=%f ylim=[%f,%f]/%d=%f'%(axw[0],axw[1],len(symbols),w,axh[0],axh[1],0,h))
     hdls=[]
-    for row,i in enumerate(symbols):
-        y=i*h+h/2
+    for i,row in enumerate(symbols):
+        x=axw[0]+i*w+w/2
         rowh=[]
         # TODO: ensure row is enumeratable...
-        for symb,j in enumerate(row):
-            x=j*w+w/2
-            txthdl =ax.text(x, y, symb, color=txtColor)
+        for j,symb in enumerate(row):
+            y=axh[0]+j*h+h/2
+            print('%s @(%f,%f)'%(symb,x,y))
+            txthdl =ax.text(x, y, symb, color=txtColor,visible=True)
             rowh.append(txthdl)
         if len(rowh)==1: rowh=rowh[0] # BODGE: ensure hdls has same structure as symbols
         hdls.append(rowh)
+    print('hds(%d)=[%s]'%(len(hdls),str(hdls)))
+    drawnow()
     return hdls
 
-
-## CONFIGURABLE VARIABLES
-verb=0
-symbols=['a' 'b' 'c' 'd']
-nSymbs =sum([len(r) for r in symbols])
-nSeq=6
-cueDuration=2
-epochDuration=.1
-interEpochDuration=.1;
-interSeqDuration=2
-
-bgColor =(.5,.5,.5)
-tgtColor=(0,1,0)
-fixColor=(1,0,0)
-txtColor=(1,1,1)
-
 # make the target sequence
-tgtSeq=list(range(nSymbs)*int(nSeq/nSymbs +1) # sequence in sequential order
+tgtSeq=list(range(nSymbs))*int(nSeq/nSymbs +1) # sequence in sequential order
 shuffle(tgtSeq) # N.B. shuffle works in-place!
 tgtSeq=tgtSeq[1:nSeq]
             
 ##--------------------- Start of the actual experiment loop ----------------------------------
 # set the display and the string for stimulus
-if DEBUG:
-    plt.switch_backend('agg') # N.B. command to work in non-display mode
 fig = plt.figure(facecolor=(0,0,0))
     
 fig.suptitle('RunSentences-Stimulus', fontsize=14, fontweight='bold',color=txtColor)
@@ -103,11 +107,13 @@ hdls=initGrid(symbols)
 ftc,hdr=bufhelp.connect();
 
 #wait for key-press to continue
-[_.set(facecolor=bgColor) for _ in hdls]
+[_.set(color=bgColor) for _ in hdls]
 txthdl.set(text='Press key to start')
 drawnow()
 waitforkey(fig)
 txthdl.set(visible=False)
+
+plt.ion()
 
 bufhelp.sendEvent('stimulus.training','start');
 ## STARTING stimulus loop
@@ -130,22 +136,24 @@ for si,tgt in enumerate(tgtSeq):
     sleep(postCueDuration)
 
     # start the scanning loop
-    for rep in range(5):
+    for rep in range(nRep):
         for si in range(nSymbs):
             # flash
             hdls[si].set(color=flashColor)
-            drawnow()
-            bufhelp.sendEvent('stimulus.cue',si)
+            bufhelp.sendEvent('stimulus.flash',si)
             bufhelp.sendEvent('stimulus.tgtFlash',si==tgt)
-            sleep(stimDuration)                
+            drawnow()
+            sleep(epochDuration)                
             # reset
             hdls[si].set(color=bgColor)
             drawnow()
-            sleep(stimDuration)
+            sleep(interEpochDuration)
 
     bufhelp.sendEvent('stimulus.trial','end');
-
+sleep(interSeqDuration)
+    
 bufhelp.sendEvent('stimulus.training','end')
-txthdl.set(text=['Thanks for taking part!' '' 'Press key to finish'],visible=True)
+[ _.set(visible=False) for _ in hdls]
+txthdl.set(text='Thanks for taking part!\nPress key to finish',visible=True)
 drawnow()
 waitforkey(fig)
