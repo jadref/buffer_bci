bbe6e376886c4bf3962940f2edc7c2282982b377
DESKTOP-REJSTIK\BrainFly 3 <d_tump@hotmail.com>
2017-11-15 11:25:50 +0100

    windows startup scripts improvements

--- eeg_quickstart.bat
+++ eeg_quickstart.bat
@@ -1,6 +1,6 @@
 setlocal enabledelayedexpansion
 set batdir=%~dp0
-cd %batdir%
+cd %batdir%\dataAcq
 
 set dataacq=%1
 if "%dataacq%"=="" ( 
@@ -10,7 +10,7 @@ set sigproc=%2
 
 echo Starting the java buffer server \(background\)
 rem wmic process call create "dataAcq/startJavaNoSaveBuffer.bat" | find "ProcessId"
-start dataAcq\startJavaBuffer.bat
+start startJavaBuffer.bat
 
 rem Weird windows hack to sleep for 2 secs to allow the buffer server to start
 ping 127.0.0.1 -n 3 > nul
@@ -18,9 +18,11 @@ ping 127.0.0.1 -n 3 > nul
 
 echo Starting the data acquisation device %dataacq% \(background\)
 if "%dataacq%"=="mobita" (
-  start dataAcq\startMobita.bat localhost 2
+  start startMobita.bat localhost 2
 ) else if "%dataacq%"=="biosemi" (
-  start dataAcq\startBiosemi.bat
+  start startBiosemi.bat
+) else if "%dataacq%"=="eego" (
+  start startEego.bat
 ) else (
   echo Dont recognise the eeg device type
 )
@@ -29,13 +31,13 @@ rem dataacqpid=$!
 if defined sigproc (
     if "%sigproc%"=="1" (
     echo Starting the default signal processing function \(background\)
-    start  .\matlab\signalProc\startSigProcBuffer.bat
+    start  ..\matlab\signalProc\startSigProcBuffer.bat
     rem sigprocpid=$!
     )
 )
 
 echo Starting the event viewer
-dataAcq\startJavaEventViewer.bat
+startJavaEventViewer.bat
 
 rem Cleanup all the processes we started
 rem TODO: make this work, getting the pid of started process seems very hard in windows....
