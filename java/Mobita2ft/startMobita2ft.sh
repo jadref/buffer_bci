df654865af171d8bef53430a80477f24c83b4114
jadref <jdrf@zepler.org>
2018-02-15 15:39:41 +0100

    fix startup script

--- java/Mobita2ft/startMobita2ft.sh
+++ java/Mobita2ft/startMobita2ft.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd `pwd`
-java -cp build/jar/Mobita2ft.jar:../../dataAcq/buffer/java/BufferClient.jar Mobita2ft.Mobita2ft
+java -cp build/jar/Mobita2ft.jar:../../dataAcq/buffer/java/BufferClient.jar nl.dcc.buffer_bci.Mobita2ft $@
