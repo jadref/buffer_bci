6a2bab4b584e64e745ef2cfc8a32cbec5a2847ef
bcicourse <jadref@gmail.com>
2018-08-20 17:50:56 +0200

    porti stuff

--- dataAcq/startPorti.bat
+++ dataAcq/startPorti.bat
@@ -1,2 +1,2 @@
 cd %~dp0
-buffer/win32/tmsi2ft_v7.exe u s 2 31 1024 tmsi_porti.cfg 1972 localhost 8000
+buffer\win32\tmsi2ft_v7.exe u s 2 31 1024 tmsi_porti_cap28.cfg 1972 localhost 8000
---
+++ porti_quickstart.bat
@@ -0,0 +1,38 @@
+setlocal enabledelayedexpansion
+set batdir=%~dp0
+cd %batdir%\dataAcq
+
+set dataacq=%1
+if "%dataacq%"=="" ( 
+set dataacq=mobita
+)
+set sigproc=%2
+
+echo Starting the java buffer server \(background\)
+rem wmic process call create "dataAcq/startJavaNoSaveBuffer.bat" | find "ProcessId"
+start startJavaBuffer.bat
+
+rem Weird windows hack to sleep for 2 secs to allow the buffer server to start
+ping 127.0.0.1 -n 3 > nul
+
+
+echo Starting the data acquisation device %dataacq% \(background\)
+start startPorti.bat
+rem dataacqpid=$!
+
+if defined sigproc (
+    if "%sigproc%"=="1" (
+    echo Starting the default signal processing function \(background\)
+    start  ..\matlab\signalProc\startSigProcBuffer.bat
+    rem sigprocpid=$!
+    )
+)
+
+echo Starting the event viewer
+startJavaEventViewer.bat
+
+rem Cleanup all the processes we started
+rem TODO: make this work, getting the pid of started process seems very hard in windows....
+rem taskkill /pid %bufferpid%
+rem taskkill /pid %dataacqpid%
+rem taskkill /pid %sigprocpid%
