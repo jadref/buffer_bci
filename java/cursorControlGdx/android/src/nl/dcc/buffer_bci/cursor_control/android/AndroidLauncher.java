e938dd0e872eae74c9b1be0412ce57a03082ae4d
jadref <jdrf@zepler.org>
2018-09-10 15:58:45 +0200

    gdx build stuff

--- java/cursorControlGdx/android/src/nl/dcc/buffer_bci/cursor_control/android/AndroidLauncher.java
+++ java/cursorControlGdx/android/src/nl/dcc/buffer_bci/cursor_control/android/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import nl.dcc.buffer_bci.cursor_control.CursorControl;
+import nl.dcc.buffer_bci.cursor_control.CursorControlGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new CursorControl(), config);
+		initialize(new CursorControlGame(), config);
 	}
 }
