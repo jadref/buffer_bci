7d07dd167b12ec02601c1f05a2a2ce75aa1bfe97
Jason Farquhar <jadref@gmail.com>
2018-12-10 18:57:50 +0100

    java-8 versions

--- dataAcq/buffer/java/compile.sh
+++ dataAcq/buffer/java/compile.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 cd `dirname $0`
+
+# force to use older java compilier
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-arm64
 # build the buffer client
 echo Building bufferclient
-javac --release 7 nl/fcdonders/fieldtrip/bufferclient/*.java
+${JAVA_HOME}/bin/javac -source 1.8 -target 1.8 nl/fcdonders/fieldtrip/bufferclient/*.java
 echo Making bufferclient.jar
 jar cf BufferClient.jar nl/fcdonders/fieldtrip/bufferclient
 
 # build the buffer server
 echo Building bufferserver
-javac --release 7 nl/fcdonders/fieldtrip/bufferserver/*.java
-javac --release 7 nl/fcdonders/fieldtrip/bufferserver/data/*.java
-javac --release 7 nl/fcdonders/fieldtrip/bufferserver/exceptions/*.java
-javac --release 7 nl/fcdonders/fieldtrip/bufferserver/network/*.java
+${JAVA_HOME}/bin/javac -source 1.8 -target 1.8 nl/fcdonders/fieldtrip/bufferserver/*.java
+${JAVA_HOME}/bin/javac -source 1.8 -target 1.8 nl/fcdonders/fieldtrip/bufferserver/data/*.java
+${JAVA_HOME}/bin/javac -source 1.8 -target 1.8 nl/fcdonders/fieldtrip/bufferserver/exceptions/*.java
+${JAVA_HOME}/bin/javac -source 1.8 -target 1.8 nl/fcdonders/fieldtrip/bufferserver/network/*.java
 echo Making bufferserver jar
 jar cfe BufferServer.jar nl.fcdonders.fieldtrip.bufferserver.BufferServer nl/fcdonders/fieldtrip/bufferserver
