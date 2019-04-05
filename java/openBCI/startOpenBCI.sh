927b644059f0d8c522a4492e55ed0e19d25739a2
Jason Farquhar <jadref@gmail.com>
2019-04-05 20:27:17 +0200

    switch serial to low-latency... to see if reduces processing lags

--- java/openBCI/startOpenBCI.sh
+++ java/openBCI/startOpenBCI.sh
@@ -5,4 +5,8 @@ if [ $# -gt 0 ]; then
 	 usbPort=$1
 	 shift
 fi
+# ensure the port is in low-latency mode to avoid annoying data delays
+echo 1 >  /sys/bus/usb-serial/devices/$usbPort/latency_timer
+setserial $usbPort low_latency
+# start the driver
 java -cp lib/BufferClient.jar:lib/jssc.jar:openBCI2ft.jar openBCI2ft $usbPort $@
