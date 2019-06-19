195f689287affa1910f51ef93a6c282e33f980d9
Jason Farquhar <jadref@gmail.com>
2019-06-19 09:51:49 +0200

    linux startup stuff

--- java/openBCI/startOpenBCI.sh
+++ java/openBCI/startOpenBCI.sh
@@ -5,8 +5,18 @@ if [ $# -gt 0 ]; then
 	 usbPort=$1
 	 shift
 fi
+buffhostport=localhost:1972
+if [ $# -gt 0 ]; then
+	 buffhostport=$1
+	 shift
+fi
+config=8
+if [ $# -gt 0 ]; then
+	 config=$1
+	 shift
+fi
 # ensure the port is in low-latency mode to avoid annoying data delays
 echo 1 >  /sys/bus/usb-serial/devices/$usbPort/latency_timer
 setserial $usbPort low_latency
 # start the driver
-java -cp lib/BufferClient.jar:lib/jssc.jar:openBCI2ft.jar openBCI2ft $usbPort $@
+java -cp ../../dataAcq/buffer/java/BufferClient.jar:lib/jssc.jar:openBCI2ft.jar openBCI2ft $usbPort $buffhostport $config $@
